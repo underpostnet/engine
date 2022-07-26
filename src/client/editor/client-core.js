9b7c0e2e50f3c3531cb8d10ef6df4bca60f6fe7c
fcoverdugo <fcoverdugoa@underpost.net>
2022-07-26 13:27:35 -0400

    update

--- src/client/editor/client-core.js
+++ src/client/editor/client-core.js
@@ -139,7 +139,7 @@ append('body', /*html*/`
             <div class='in container' style='text-align: right'>
                 Source Code
                 <img src='/assets/github.png' class='inl' style='width: 20px; top: 5px'> 
-                <a href='https://github.com/underpostnet/editor'>GitHub</a>
+                <a href='https://github.com/underpostnet/underpost-engine'>GitHub</a>
                 <br>
                 Developed By
                 <img src='/assets/underpost.png' class='inl' style='width: 23px; top: 5px; left: 3px'> 
