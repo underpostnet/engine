142c8889cd403c6edf70e2ebcbc98f6573f976ef
fcoverdugo <fcoverdugoa@underpost.net>
2022-07-27 11:08:01 -0400

    update

--- src/server.js
+++ src/server.js
@@ -4,17 +4,17 @@
 import express from 'express';
 
 // server modules
-import { morganMiddleware } from '../../modules/morgan.js';
-import { logger } from '../../modules/logger.js';
+import { morganMiddleware } from './modules/morgan.js';
+import { logger } from './modules/logger.js';
 
 // buil dev env
 import { buildDev } from './build-dev.js';
 // api
-import { apiUtil } from '../../api/util.js';
+import { apiUtil } from './api/util.js';
 // server side client render
-import { ssr } from '../../client/ssr.js';
+import { ssr } from './client/ssr.js';
 // views
-import { viewMetaData, viewPaths } from '../../client/engine/server-render.js';
+import { viewMetaData, viewPaths } from './client/engine/server-render.js';
 
 import dotenv from 'dotenv';
 
