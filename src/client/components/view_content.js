3903843ad833c5b2c048f9feeef480f8f61a3676
fcoverdugo <fcoverdugoa@underpost.net>
2022-08-11 13:20:25 -0400

    update

--- src/client/components/view_content.js
+++ src/client/components/view_content.js
@@ -12,7 +12,8 @@ this.view_content = {
 
                 const requestResult = await serviceRequest(() => `/uploads${GLOBAL['current-view-content'].static}`);
                 console.log('view content file', requestResult);
-                if (GLOBAL[GLOBAL['current-view-content'].component].renderView)
+                if (GLOBAL[GLOBAL['current-view-content'].component].renderView) {
+                    const idEdit = 'x' + s4();
                     htmls('render-view-content',
                         /*html*/`
                         <div class='in container title'>
@@ -20,12 +21,25 @@ this.view_content = {
                         </div>
                         <div class='in container'>
                             ${GLOBAL['current-view-content'].date.replace('T', ' ').slice(0, -8)}
+                            <br>
+                            <button class='${idEdit}'> ${renderLang({ es: 'Editar', en: 'Edit' })} </button>
                         </div>
                         <div class='in container'>
                             ${GLOBAL[GLOBAL['current-view-content'].component].renderView(GLOBAL['current-view-content'], requestResult)}
                         </div>                            
                         `
                     );
+                    s('.' + idEdit).onclick = () => {
+                        GLOBAL['current-edit-content'] = newInstance(GLOBAL['current-view-content']);
+                        GLOBAL['current-edit-content'].raw = requestResult;
+                        GLOBAL.router(
+                            {
+                                newPath: viewPaths.find(x => x.component == GLOBAL['current-edit-content'].component).path
+                            }
+                        );
+                    };
+
+                }
 
             })();
 
