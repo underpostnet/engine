f908791c32f7b34cf2da1a20d3d75f768c72fd48
fcoverdugo <fcoverdugoa@underpost.net>
2022-08-25 10:38:05 -0400

    update

--- src/client/core/router.js
+++ src/client/core/router.js
@@ -3,55 +3,18 @@ this.router = options => {
     let valid = false;
     let testEvalPath;
     try {
-        testEvalPath = options.newPath;
-        // -----------------------------------------------------------------------
-        // params uri validator --------------------------------------------------
-        const uriParam = testEvalPath.split('/').pop().split(':').pop();
-        if (localStorage.getItem(uriParam)) {
-            testEvalPath = testEvalPath.replace(`:${uriParam}`, localStorage.getItem(uriParam));
-        }
-        // -----------------------------------------------------------------------
-        // -----------------------------------------------------------------------
+        // params uri validator
+        testEvalPath = validatePreUriParams(options.newPath);
     } catch (error) {
         testEvalPath = view.path;
     }
     if (testEvalPath == '') testEvalPath = '/';
     viewPaths.map((path, i) => {
 
-        // -----------------------------------------------------------------------
-        // params uri validator --------------------------------------------------
-        // clear uri
-        // getURI -> .split('?')[0].split('#')[0]
-
-        if (path.path.split('/').pop()[0] == ':') {
-            const uriParam = path.path.split('/').pop().split(':').pop();
-
-            // console.log(uriParam == testEvalPath.split('/').pop().split(':').pop());
-            // console.log(testEvalPath.split('/').pop() == localStorage.getItem(uriParam));
-            // console.log(uriParam == getURI().split('/').pop().split(':').pop());
-            // console.log(getURI().split('/').pop() == localStorage.getItem(uriParam));
-
-            if (
-                localStorage.getItem(uriParam)
-                &&
-                (
-                    getURI().split('/').pop() == localStorage.getItem(uriParam)
-                    ||
-                    testEvalPath.split('/').pop() == localStorage.getItem(uriParam)
-                )
-            ) {
-                console.error('ROUTER: fix param path');
-                const paramPath = path.path.replace(`:${uriParam}`, localStorage.getItem(uriParam));
-                testEvalPath = paramPath;
-                path.path = paramPath;
-            } else {
-                // se esta buscando un usuario publico verificar contenido publico
-                // de lo contrario dedirect to 404
-                console.error('ROUTER: public user dashboard?');
-            }
-        }
-        // -----------------------------------------------------------------------
-        // -----------------------------------------------------------------------
+        // params uri validator
+        const VUP = validateUriParams(path, testEvalPath);
+        testEvalPath = VUP.testEvalPath;
+        path = VUP.path;
 
         const testIncludesHome = path.homePaths.includes(testEvalPath);
         const validPath = path.path == testEvalPath;
@@ -85,6 +48,47 @@ this.router = options => {
 };
 
 const buildBaseUri = () => dev ? `/${viewMetaData.clientID}` : '';
+
+const validatePreUriParams = testEvalPath => {
+    const uriParam = testEvalPath.split('/').pop().split(':').pop();
+    if (localStorage.getItem(uriParam)) {
+        testEvalPath = testEvalPath.replace(`:${uriParam}`, localStorage.getItem(uriParam));
+    }
+    return testEvalPath;
+};
+
+const validateUriParams = (path, testEvalPath) => {
+    if (path.path.split('/').pop()[0] == ':') {
+        const uriParam = path.path.split('/').pop().split(':').pop();
+
+        // console.log(uriParam == testEvalPath.split('/').pop().split(':').pop());
+        // console.log(testEvalPath.split('/').pop() == localStorage.getItem(uriParam));
+        // console.log(uriParam == getURI().split('/').pop().split(':').pop());
+        // console.log(getURI().split('/').pop() == localStorage.getItem(uriParam));
+
+        if (
+            localStorage.getItem(uriParam)
+            &&
+            (
+                getURI().split('?')[0].split('#')[0].split('/').pop() == localStorage.getItem(uriParam)
+                ||
+                testEvalPath.split('/').pop() == localStorage.getItem(uriParam)
+            )
+        ) {
+            console.error('ROUTER: fix param path');
+            const paramPath = path.path.replace(`:${uriParam}`, localStorage.getItem(uriParam));
+            testEvalPath = paramPath;
+            path.path = paramPath;
+        } else {
+            // se esta buscando un usuario publico verificar contenido publico
+            // de lo contrario dedirect to 404
+            console.error('ROUTER: public user dashboard?');
+        }
+    }
+    return {
+        testEvalPath, path
+    }
+};
 
 this.router();
 
