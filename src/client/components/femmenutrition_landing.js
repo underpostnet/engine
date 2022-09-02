8cb3be44f4c2b988beb7fcc0670600808e2e8d72
fcoverdugo <fcoverdugoa@underpost.net>
2022-09-02 17:48:06 -0400

    update

--- src/client/components/femmenutrition_landing.js
+++ src/client/components/femmenutrition_landing.js
@@ -1,7 +1,49 @@
 
 
 this.femmenutrition_landing = {
+    render3Blocks: dataBlocks => /*html*/`
+    <div class='in fll cell-l2-a' style='${rrb()}'>
+
+        <div class='in' style='height: 70px; ${rrb()}'>
+            <div class='abs center' 
+            style='
+            font-size: 20px;
+            font-weight: bold;'
+            >${dataBlocks.title}</div>
+        </div>
+        
+        <div class='in' style='height: 80px; ${rrb()}'>
+            <img class='abs center' src='${dataBlocks.img}' style='width: 15%; max-width: 60px;'>
+        </div>
+
+        <div class='in' style='height: 100px; ${rrb()}'>
+            <div class='abs center'
+            style='font-size: 20px'
+            >${dataBlocks.desc}</div>
+        </div>
+
+    </div>
+    `,
     init: function () {
+
+
+        const L2_DATA = [
+            {
+                title: 'Alta Calidad Nutricional',
+                img: '/assets/apps/femmenutrition/l2-icons/tarjeta.png',
+                desc: 'Productos formulados en base a estándares dietéticos'
+            },
+            {
+                title: 'Formulado por Expertas ',
+                img: '/assets/apps/femmenutrition/l2-icons/mujer.png',
+                desc: 'Liderado por un equipo de mujeres profesionales y especialistas '
+            },
+            {
+                title: 'Adecuado 100% para Nosotras',
+                img: '/assets/apps/femmenutrition/l2-icons/lab.png',
+                desc: 'Planificados para cubrir las necesidades nutricionales femeninas'
+            }
+        ];
 
 
         return /*html*/`
@@ -27,6 +69,9 @@ this.femmenutrition_landing = {
                 .cell-l1-b {
                     width: 100%;
                 }
+                .cell-l2-a {
+                    width: 100%;
+                }
              `},
             {
                 limit: mobileLimit,
@@ -36,6 +81,9 @@ this.femmenutrition_landing = {
                 }
                 .cell-l1-b {
                     width: 40%;
+                }
+                .cell-l2-a {
+                    width: 33.3%;
                 }
              `}
         ])}
@@ -63,6 +111,12 @@ this.femmenutrition_landing = {
                                 <img class='in img-l1' src='/assets/apps/femmenutrition/lp1.png'>
                         </div>
                 </div>            
+            </div>
+
+            <div class='in container' style='background: #FFDEE1; color: #ed5c6b;'>
+                <div class='fl'>
+                    ${L2_DATA.map(x => this.render3Blocks(x)).join('')}
+                </div>
             </div>
         
         `
