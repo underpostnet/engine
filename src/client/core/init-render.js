94c7111d4f2da35d1c1279c7cb15ca0c83e71e12
fcoverdugo <fcoverdugoa@underpost.net>
2022-09-08 16:54:40 -0400

    _top

--- src/client/core/init-render.js
+++ src/client/core/init-render.js
@@ -6,7 +6,7 @@ this.init_render = (options) => {
         if (getQueryParams().lang) s('html').lang = getQueryParams().lang;
         htmls('body', /*html*/`
         <top-banner></top-banner>
-        <a ${getQueryParams().target == '_blank' ? `target='_blank'` : ''} alt='${viewMetaData.mainTitle}' href='${buildBaseUri()}/' style='text-decoration: none'>
+        <a target='_top' alt='${viewMetaData.mainTitle}' href='${buildBaseUri()}/' style='text-decoration: none'>
                 ${banner() == '' ?/*html*/`
                 <div class='in container banner' style='${borderChar(1, 'white')}'>
                         ${viewMetaData.mainTitle}
@@ -29,14 +29,14 @@ this.init_render = (options) => {
         ${footer() == '' ?/*html*/`
         <footer class='fl container'>
                 <div class='in flr'>
-                        <a target='_blank' href='https://github.com/underpostnet/underpost-engine'> 
+                        <a target='_top' href='https://github.com/underpostnet/underpost-engine'> 
                         <img src='/assets/common/github.png' class='inl' style='width: 20px; top: 5px'> 
                         <!-- v${version} -->
                         </a>                      
                 </div>   
                 <div class='in fll'>
                         <img class='inl' style='width: 20px; top: 3px' src='https://www.dogmadual.com/favicon.ico' alt='DOGMADUAL'>
-                        <a target='_blank' href='https://www.dogmadual.com/'>DOGMADUAL.com</a>
+                        <a target='_top' href='https://www.dogmadual.com/'>DOGMADUAL.com</a>
                 </div>     
         </footer>     
         `: footer()}   
