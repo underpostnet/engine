1dc9c73244e580298bd0ab286dafd6d725a404b8
fcoverdugo <fcoverdugoa@underpost.net>
2022-09-09 10:44:09 -0400

    update

--- src/client/components/home_dev.js
+++ src/client/components/home_dev.js
@@ -6,11 +6,24 @@ this.home_dev = {
 
 
         return /*html*/`
-        
-                    <div class='in container'>
-                            Hellow World
+                    <div class='in container title' style='text-align: center'>
+                        current projects
                     </div>
-        
+                    <style>
+                        .icon-app {
+                            width: 40px;
+                            height: 40px;
+                            top: 7px;
+                        }
+                    </style>
+                    ${APPS.map((dataApp, i) => /*html*/ i != 0 ? `                    
+                        <a href='${dataApp.baseHome}' style='text-decoration: none; font-size: 30px'>
+                                <div class='in container'>
+                                    <img class='inl icon-app' src='${dataApp.viewMetaData.favicon.path}'>
+                                    ${dataApp.viewMetaData.mainTitle}
+                                </div>
+                        </a>                    
+                    `: '').join('')}
         
         `
     }
