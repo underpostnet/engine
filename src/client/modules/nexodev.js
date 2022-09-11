fd3a1356208723c178f89b2c7199476551069c8f
fcoverdugo <fcoverdugoa@underpost.net>
2022-09-10 23:13:57 -0400

    update

--- src/client/modules/nexodev.js
+++ src/client/modules/nexodev.js
@@ -46,14 +46,7 @@ const viewMetaData = {
     statics: [
         ['/assets/apps/nexodev', `./src/client/assets/apps/nexodev`]
     ],
-    fonts: [
-        {
-            name: 'gothic',
-            src: '/fonts-underpost/gothic/GOTHIC.ttf',
-            format: 'truetype', // opentype
-            activesClass: ['body', 'button']
-        }
-    ]
+    fonts: []
 };
 
 const baseHome = '/' + clientID;
