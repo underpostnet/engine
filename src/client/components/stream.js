c9be9d9978d8497a08c9c48f4163c82830f859c4
fcoverdugo <fcoverdugoa@underpost.net>
2022-09-17 15:21:50 -0400

    update

--- src/client/components/stream.js
+++ src/client/components/stream.js
@@ -9,11 +9,41 @@ this.stream = {
 
 
 
-        // const ioSocket = io('/');
-        // const peerInstance = new Peer(undefined, {
-        //     host: '/',
-        //     port: 5501
-        // });
+        const ioSocket = io('/');
+        const peerInstance = new Peer(undefined, {
+            host: '/',
+            port: 5501
+        });
+
+        peerInstance.on('open', id => {
+
+            console.warn('peerInstance | open', id);
+
+            ioSocket.emit('join-room', 0, id, 1); // id room , id peer, id type
+
+            // s('.'+mp3_actual).captureStream();
+            // peerInstance.call(id, stream_radio);
+
+        });
+
+        peerInstance.on('disconnected', () => {
+
+            console.log('peerInstance | disconnect');
+
+        });
+
+        ioSocket.on('receiver-connected', id => {
+
+            console.warn('ioSocket | receiver-connected', id);
+
+
+        });
+
+        ioSocket.on('disconnect', () => {
+
+            console.log('ioSocket | disconnect');
+
+        });
 
 
 
--- src/modules/socket.io.js
+++ src/modules/socket.io.js
@@ -5,6 +5,20 @@ import { Server, Socket } from 'socket.io';
 import dotenv from 'dotenv';
 import httpServer from 'http';
 
+// import httpProxy from 'http-proxy';
+// httpProxy
+// .createProxyServer({
+//     target: `http://localhost:5501/socket.io`,
+//     ws: true,
+// })
+// .listen(5502);
+
+// RewriteEngine On
+// RewriteCond %{REQUEST_URI}  ^/socket.io            [NC]
+// RewriteCond %{QUERY_STRING} transport=websocket    [NC]
+// # RewriteCond %{HTTP:Upgrade} =websocket [NC]
+// RewriteRule /(.*)           ws://www.cyberiaonline.com:3001/$1 [P,L]
+
 dotenv.config();
 
 const ioModule = app => {
@@ -31,6 +45,13 @@ const ioModule = app => {
         //     cert: fs.readFileSync((data.sslCertPath)),
         //     ca: fs.readFileSync((data.sslCaPath))
         // }
+    });
+
+
+    app.get('/socket.io', (req, res) => {
+
+
+        return res.end('');
     });
 
     return { io, peerServer };
