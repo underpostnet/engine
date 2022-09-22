85b45f8c19e3fa3168e72d5b4dd3f1d00573f4ea
fcoverdugo <fcoverdugoa@underpost.net>
2022-09-21 21:41:13 -0400

    update

--- src/client/components/stream.js
+++ src/client/components/stream.js
@@ -4,83 +4,70 @@
 this.stream = {
     init: function () {
 
+        setTimeout(() => {
+            return;
+
+            const ROOM_ID = 'test-room';
+            const socket = io('http://localhost:5501'); // Create our socket
+            const videoGrid = s('.video-grid'); // Find the Video-Grid element
+
+            const myPeer = new Peer() // Creating a peer element which represents the current user
+            const myVideo = document.createElement('video') // Create a new video tag to show our video
+            myVideo.muted = true // Mute ourselves on our end so there is no feedback loop
+
+            // Access the user's video and audio
+            navigator.mediaDevices.getUserMedia({
+                video: true,
+                audio: true
+            }).then(stream => {
+                addVideoStream(myVideo, stream) // Display our video to ourselves
+
+                myPeer.on('call', call => { // When we join someone's room we will receive a call from them
+                    call.answer(stream) // Stream them our video/audio
+                    const video = document.createElement('video') // Create a video tag for them
+                    call.on('stream', userVideoStream => { // When we recieve their stream
+                        addVideoStream(video, userVideoStream) // Display their video to ourselves
+                    })
+                })
+
+                socket.on('user-connected', userId => { // If a new user connect
+                    connectToNewUser(userId, stream)
+                })
+            })
+
+            myPeer.on('open', id => { // When we first open the app, have us join a room
+                socket.emit('join-room', ROOM_ID, id)
+            })
+
+            function connectToNewUser(userId, stream) { // This runs when someone joins our room
+                const call = myPeer.call(userId, stream) // Call the user who just joined
+                // Add their video
+                const video = document.createElement('video')
+                call.on('stream', userVideoStream => {
+                    addVideoStream(video, userVideoStream)
+                })
+                // If they leave, remove their video
+                call.on('close', () => {
+                    video.remove()
+                })
+            }
 
 
-
-        this.audioElementId = 'x' + s4();
-        this.audioReceptorElement = 'x' + s4();
-        this.streamEmiter = false;
-
-        const ioSocket = io('/');
-        const peerInstance = new Peer(undefined, {
-            host: '/',
-            port: 5501
+            function addVideoStream(video, stream) {
+                video.srcObject = stream
+                video.addEventListener('loadedmetadata', () => { // Play the video as it loads
+                    video.play()
+                })
+                videoGrid.append(video) // Append video element to videoGrid
+            }
         });
-
-        peerInstance.on('open', id => {
-
-            console.warn('peerInstance | open', id);
-
-            ioSocket.emit('join-room', 0, id, 1); // id room , id peer, id type
-
-            s('.' + this.audioElementId).onplay = () => {
-                this.streamEmiter = true;
-                peerInstance.call(id, s('.' + this.audioElementId).captureStream());
-            };
-
-        });
-
-
-        peerInstance.on('call', call => {
-            call.answer(new MediaStream());
-
-            call.on('stream', stream => {
-
-                if (!this.streamEmiter) {
-                    s('.' + this.audioReceptorElement).srcObject = stream;
-                    s('.' + this.audioReceptorElement).play();
-                }
-
-
-            });
-
-        });
-
-        peerInstance.on('disconnected', () => {
-
-            console.log('peerInstance | disconnect');
-
-        });
-
-        ioSocket.on('receiver-connected', id => {
-
-            console.warn('ioSocket | receiver-connected', id);
-
-
-        });
-
-        ioSocket.on('disconnect', () => {
-
-            console.log('ioSocket | disconnect');
-
-        });
-
-
-
-
 
         return /*html*/`
             <div class='in container'>
-                <audio controls class='${this.audioElementId}'>
-                    <source 
-                        src="/uploads/cloud/francisco-verdugo/f955e-Carpenter-Brut-Leather-Teeth-Accelerated_pFGI5UMlIRQ.mp3" 
-                        type="audio/mpeg">
-                </audio>
+                   <div class='in video-grid'></div>
             </div>
             
-            <div class='in container'>
-                <audio controls class='${this.audioReceptorElement}'></audio>
-            </div>
+           
         
         `
     }
--- src/modules/socket.io.js
+++ src/modules/socket.io.js
@@ -1,78 +1,37 @@
 
 
-import { PeerServer } from 'peer';
-import { Server, Socket } from 'socket.io';
+import { Server } from 'socket.io';
 import dotenv from 'dotenv';
-import httpServer from 'http';
-
-// import { Server } from 'socket.io-p2p-server';
-// https://dev.to/codesphere/building-a-video-chat-app-with-socket-io-peerjs-codesphere-5a63
-// https://stackoverflow.com/questions/21629752/using-node-http-proxy-to-proxy-websocket-connections
-// expressjs websocket proxy
-
-// const proxy = httpProxy.createProxyServer({ ws: true });
-
-// import httpProxy from 'http-proxy';
-// httpProxy
-// .createProxyServer({
-//     target: `http://localhost:5501/socket.io`,
-//     ws: true,
-// })
-// .listen(5502);
-
-// RewriteEngine On
-// RewriteCond %{REQUEST_URI}  ^/socket.io            [NC]
-// RewriteCond %{QUERY_STRING} transport=websocket    [NC]
-// # RewriteCond %{HTTP:Upgrade} =websocket [NC]
-// RewriteRule /(.*)           ws://www.cyberiaonline.com:3001/$1 [P,L]
 
 dotenv.config();
 
 const ioModule = app => {
 
-    const io = new Server(httpServer.Server(app));
-    // io.use(Server);
+    const io = new Server(process.env.IO_PORT, {
+        cors: {
+            origin: `http://localhost:${process.env.PORT}`,
+            methods: ['GET', 'POST'],
+            //   allowedHeaders: ['my-custom-header'],
+            //   credentials: true
+        }
+    });
 
+    // When someone connects to the server
     io.on('connection', socket => {
-        socket.on('join-room', (roomid, peerid, type) => {
-            socket.join(roomid);
-            // socket.to(roomid).broadcast.emit(type === 1 ? "sender-connected" : "receiver-connected", peerid);
-            socket.to(roomid).emit(type === 1 ? "sender-connected" : "receiver-connected", peerid);
+        console.log('io connection', socket);
+        // When someone attempts to join the room
+        socket.on('join-room', (roomId, userId) => {
+            socket.join(roomId)  // Join the room
+            socket.broadcast.emit('user-connected', userId) // Tell everyone else in the room that we joined
 
+            // Communicate the disconnection
             socket.on('disconnect', () => {
-                // socket.to(roomid).broadcast.emit(type === 1 ? "sender-disconnected" : "receiver-disconnected", peerid);
-                socket.to(roomid).emit(type === 1 ? "sender-disconnected" : "receiver-disconnected", peerid);
-            });
-        });
+                socket.broadcast.emit('user-disconnected', userId)
+            })
+        })
     });
 
-    const peerServer = PeerServer({
-        port: process.env.PEER_PORT
-        // ssl: {
-        //     key: fs.readFileSync((data.sslKeyPath)),
-        //     cert: fs.readFileSync((data.sslCertPath)),
-        //     ca: fs.readFileSync((data.sslCaPath))
-        // }
-    });
-
-
-    // // proxy HTTP GET / POST
-    // app.get('/socket.io/*', function (req, res) {
-    //     // console.log("proxying GET request", req.url);
-    //     proxy.web(req, res, { target: 'http://localhost:5500' });
-    // });
-    // app.post('/socket.io/*', function (req, res) {
-    //     // console.log("proxying POST request", req.url);
-    //     proxy.web(req, res, { target: 'http://localhost:5500' });
-    // });
-
-    // // Proxy websockets
-    // app.on('upgrade', function (req, socket, head) {
-    //     console.log("proxying upgrade request", req.url);
-    //     proxy.ws(req, socket, head);
-    // });
-
-    return { io, peerServer };
+    return { io };
 
 };
 
