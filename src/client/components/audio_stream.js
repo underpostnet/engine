1315366be104e22313117f981f37affa43c4bd7c
fcoverdugo <fcoverdugoa@underpost.net>
2022-09-27 19:24:58 -0400

    update

--- src/client/components/audio_stream.js
+++ src/client/components/audio_stream.js
@@ -20,9 +20,15 @@ this.audio_stream = {
             }
 
             const ROOM_ID = 'test-room';
+            const connectionOptions =  {
+                "force new connection" : true,
+                "reconnectionAttempts": "Infinity", 
+                "timeout" : 10000,                  
+                "transports" : ["websocket"] // 'polling', 'flashsocket'
+              };
             GLOBAL.audio_stream.socket = io(
                 dev ? 'http://localhost:5501' : 'https://www.cyberiaonline.com/stream',
-                { transports: ['websocket', 'polling', 'flashsocket'] }
+                connectionOptions
             ); // Create our socket
             GLOBAL.audio_stream.myPeer = new Peer(); // Creating a peer element which represents the current user
 
