7081f697337b26c066f498d22a888b87b88baf93
fcoverdugo <fcoverdugoa@underpost.net>
2022-09-27 18:59:13 -0400

    update

--- src/modules/socket.io.js
+++ src/modules/socket.io.js
@@ -12,10 +12,9 @@ const ioModule = app => {
             // origin: `http://localhost:${process.env.PORT}`,
             origins: process.env.NODE_ENV == 'development' ?
                 [`http://localhost:${process.env.PORT}`, 'http://localhost:3001'] :
-                // [`https://www.cyberiaonline.com`, 'https://underpost.net'],
-                [`http://www.cyberiaonlibe.com:${process.env.IO_PORT}`],
+                [`https://www.cyberiaonline.com`, 'https://underpost.net', `http://www.cyberiaonlibe.com:${process.env.IO_PORT}`],
             methods: ['GET', 'POST'],
-            //   allowedHeaders: ['my-custom-header'],
+            allowedHeaders: ['Access-Control-Allow-Origin'],
             //   credentials: true
         }
     });
