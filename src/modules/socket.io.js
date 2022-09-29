095072d503009bdf96462b67c28161f00cd33737
fcoverdugo <fcoverdugoa@underpost.net>
2022-09-29 10:36:29 -0300

    update

--- src/modules/socket.io.js
+++ src/modules/socket.io.js
@@ -95,7 +95,7 @@ const ioModule = (app, options) => {
 
     peerServer.use(function (req, res, next) {
         res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         next();
     });
 
