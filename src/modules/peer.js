e1a74379387f7d626583d602f4d031a664d05db1
fcoverdugo <fcoverdugoa@underpost.net>
2022-10-06 11:32:35 -0300

    update

--- src/modules/peer.js
+++ src/modules/peer.js
@@ -4,7 +4,16 @@ import { logger } from './logger.js';
 
 dotenv.config();
 
-const peerOptions = { port: process.env.PEER_PORT };
+const peerOptions = {
+    port: process.env.PEER_PORT,
+    /* proxied: true */
+};
+
+if (process.env.NODE_ENV != 'development') peerOptions.ssl = {
+    key: fs.readFileSync('C:/dd/virtual_machine/SSL/services_cyberiaonline/ssl/key.key'),
+    cert: fs.readFileSync('C:/dd/virtual_machine/SSL/services_cyberiaonline/ssl/crt.crt'),
+    ca: fs.readFileSync('C:/dd/virtual_machine/SSL/services_cyberiaonline/ssl/ca_bundle.crt')
+};
 
 const peerServer = app =>
     PeerServer(peerOptions, () => {
