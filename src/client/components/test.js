725e4764674369a15c494aad0422fa1e63f58c4a
fcoverdugo <fcoverdugoa@underpost.net>
2022-10-29 20:25:47 -0300

    update

--- src/client/components/test.js
+++ src/client/components/test.js
@@ -10,31 +10,48 @@ this.test = {
 
         const idAvatar = `avatar-${s4()}`;
 
+        let legRotateR = true;
+        let legRotateL = true;
+        const getDegLeg = type => {
+            if (type == 'r') {
+                if (legRotateR) {
+                    legRotateR = false;
+                    return 45;
+                }
+                legRotateR = true;
+                return 0;
+            } else {
+                if (legRotateL) {
+                    legRotateL = false;
+                    return 0;
+                }
+                legRotateL = true;
+                return 45;
+            }
+        };
         setInterval(() => {
 
 
             htmls(`.${idAvatar}-leg-r-animation`, /*css*/`
                 .${idAvatar}-leg-r {
-                    left: 60%;
-                    transform: rotate(${random(-90, 90)}deg);
+                    transform: rotate(${getDegLeg('r')}deg);
                 }
             `);
             htmls(`.${idAvatar}-leg-l-animation`, /*css*/`
                 .${idAvatar}-leg-l {
-                    left: 40%;
-                    transform: rotate(${random(-90, 90)}deg);
+                    transform: rotate(${getDegLeg('l')}deg);
                 }
             `);
 
 
-        }, 1000);
+        }, 500);
 
         return /*html*/`
         <style>
             .${idAvatar}-content {
-                width: 300px;
-                height: 600px;
-                border: 2px solid white;
+                width: 150px;
+                height: 200px;
+                background: gray;
                 margin: 5px;
             }
             .${idAvatar}-leg-r, .${idAvatar}-leg-l {
@@ -43,7 +60,8 @@ this.test = {
                 height: 30%;
                 border-radius: 40%;
                 transition: .3s;
-                top: 60%;
+                top: 60%;                
+                left: 45%;
             }
             .${idAvatar}-leg-l {
                 transform-origin: top left;
@@ -54,13 +72,11 @@ this.test = {
         </style>
         <style class='${idAvatar}-leg-r-animation'>
             .${idAvatar}-leg-r {
-                left: 60%;
                 transform: rotate(0deg);
             }
         </style>
         <style class='${idAvatar}-leg-l-animation'> 
             .${idAvatar}-leg-l {
-                left: 40%;
                 transform: rotate(0deg);
             }
         </style>
