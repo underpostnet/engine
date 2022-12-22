92145d78a868f6c5b0b81fac80679a26bb9f7133
fcoverdugo <fcoverdugoa@underpost.net>
2022-12-22 19:55:18 -0300

    update

--- src/client/components/test.js
+++ src/client/components/test.js
@@ -16,6 +16,7 @@ this.test = {
                 this.x = random(0, 100);
                 this.y = random(0, 100);
                 this.container = options.container;
+                this.vel = 10;
                 append(this.container, /*html*/`
                         <style class='${this.id}'></style>
                         <style>
@@ -28,6 +29,38 @@ this.test = {
                         </style>
                         <${this.id} class='abs'></${this.id}>
                 `);
+                if (this.ArrowLeft) stopListenKey(this.ArrowLeft);
+                this.ArrowLeft = startListenKey({
+                    key: 'ArrowLeft',
+                    vel: this.vel,
+                    onKey: () => {
+                        this.y--;
+                    }
+                });
+                if (this.ArrowRight) stopListenKey(this.ArrowRight);
+                this.ArrowRight = startListenKey({
+                    key: 'ArrowRight',
+                    vel: this.vel,
+                    onKey: () => {
+                        this.y++;
+                    }
+                });
+                if (this.ArrowUp) stopListenKey(this.ArrowUp);
+                this.ArrowUp = startListenKey({
+                    key: 'ArrowUp',
+                    vel: this.vel,
+                    onKey: () => {
+                        this.x--;
+                    }
+                });
+                if (this.ArrowDown) stopListenKey(this.ArrowDown);
+                this.ArrowDown = startListenKey({
+                    key: 'ArrowDown',
+                    vel: this.vel,
+                    onKey: () => {
+                        this.x++;
+                    }
+                });
                 return this;
             },
             loop: function () {
