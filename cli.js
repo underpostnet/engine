9a8acc1103ae45a708803e18d8d8c80c0291be6a
fcoverdugo <fcoverdugoa@underpost.net>
2022-12-24 11:18:11 -0300

    update

---
+++ cli.js
@@ -0,0 +1,4 @@
+import { UnderPostManager } from "./underpost_modules/underpost.net/underpost-modules-v1/underpost-manager/class/UnderPostManager.js";
+
+
+new UnderPostManager('./data/cryptokoyn').init();
