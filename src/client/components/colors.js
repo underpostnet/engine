4a793e88620a13f0fd57ee977fc47d87390c7618
fcoverdugo <fcoverdugoa@underpost.net>
2023-01-08 10:48:36 -0300

    update

--- src/client/components/colors.js
+++ src/client/components/colors.js
@@ -12,15 +12,42 @@ this.colors = {
                     color: white;
                     font-size: 20px;
                     font-weight: bold;
+                    border-radius: 10px;
                     ${borderChar(2, 'black')}
                 }
             </style> 
             <div class='in container'>
-                    ${renderColors.map(dataColor => /*html*/`
+                    ${renderColors.map(dataColor => {
+            const idCopyHex = 'x' + s4() + s4();
+            const idCopyName = 'x' + s4() + s4();
+            setTimeout(() => {
+                s(`.${idCopyHex}`).onclick = async () => {
+                    await copyData(dataColor.hex);
+                    append('body', renderFixModal({
+                        id: 'mini-modal-' + s4(),
+                        icon: sucessIcon,
+                        color: 'green',
+                        content: renderLang({ es: 'Contenido Copiado al Portapapeles', en: 'Copy to Clipboard' })
+                    }));
+                };
+                s(`.${idCopyName}`).onclick = async () => {
+                    await copyData(dataColor.name.toLowerCase());
+                    append('body', renderFixModal({
+                        id: 'mini-modal-' + s4(),
+                        icon: sucessIcon,
+                        color: 'green',
+                        content: renderLang({ es: 'Contenido Copiado al Portapapeles', en: 'Copy to Clipboard' })
+                    }));
+                };
+            });
+            return    /*html*/`
                         <div class='inl content-color' style='background: ${dataColor.hex};'>
-                            ${dataColor.name.toLowerCase()}
+                            ${dataColor.name.toLowerCase()} <i class='fas fa-copy ${idCopyName}'></i>
+                            <br>
+                            ${dataColor.hex} <i class='fas fa-copy ${idCopyHex}'></i>
                         </div>
-                    `).join('')}
+                    `
+        }).join('')}
             </div>
         `
     }
