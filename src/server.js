e4a2d83fbe3c0b18f4ad006619463799c36a3720
fcoverdugo <fcoverdugoa@underpost.net>
2023-10-21 09:02:38 -0300

    update

--- src/server.js
+++ src/server.js
@@ -1,9 +1,26 @@
 'use strict';
 
+// https://nodejs.org/api
+// https://expressjs.com/en/4x/api.html
+
+import dotenv from 'dotenv';
+import isAdmin from 'is-admin';
+
+import { loggerFactory } from './server/logger.js';
 import { buildClient } from './server/client-build.js';
 import { buildRuntime } from './server/runtime.js';
 import { buildProxy } from './server/proxy.js';
+import { Dns } from './server/dns.js';
+
+dotenv.config();
+
+const logger = loggerFactory(import.meta);
+
+logger.info('argv', process.argv);
+logger.info('env', process.env.NODE_ENV);
+logger.info('admin', await isAdmin());
 
 await buildClient();
 await buildRuntime();
 await buildProxy();
+await Dns.InitIpDaemon();
