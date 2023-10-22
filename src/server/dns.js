dae5aaae05019277eacf3bf99af07b667a2505c7
fcoverdugo <fcoverdugoa@underpost.net>
2023-10-22 13:58:06 -0300

    update

--- src/server/dns.js
+++ src/server/dns.js
@@ -14,8 +14,7 @@ const Dns = {
   ipDaemon: null,
   InitIpDaemon: async function () {
     // DNS Records: [ANAME] -> [A] -> [ip]
-    const confDnsPath =
-      process.env.NODE_ENV === 'production' ? './engine-private/conf.dns.private.json' : './src/conf.dns.json';
+    const confDnsPath = './src/conf.dns.json';
     let confDnsData = JSON.parse(fs.readFileSync(confDnsPath, 'utf8'));
     this.ip = confDnsData.ipDaemon.ip;
     logger.info(`Current ip`, this.ip);
