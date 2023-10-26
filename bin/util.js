6a71e63b9e5671afe80e6467f44cfc6f3e3e73fa
fcoverdugo <fcoverdugoa@underpost.net>
2023-10-26 01:26:35 -0300

    update

--- bin/util.js
+++ bin/util.js
@@ -1,6 +1,8 @@
 import fs from 'fs-extra';
 import { loggerFactory } from '../src/server/logger.js';
 import { shellExec } from '../src/server/process.js';
+import { range } from '../src/client/components/core/CommonJs.js';
+import { network } from '../src/server/network.js';
 const logger = loggerFactory(import.meta);
 
 logger.info('argv', process.argv);
@@ -26,6 +28,21 @@ try {
       break;
     case 'export-vs-extensions':
       shellExec(`code --list-extensions > vs-extensions.txt`);
+      break;
+    case 'kill-ports':
+      if (!process.argv[3]) process.argv[3] = '22,80,443,3000-3020';
+      for (const port of process.argv[3].split(',')) {
+        const rangePort = port.split('-');
+        if (rangePort[1])
+          for (const port of range(parseInt(rangePort[0]), parseInt(rangePort[1]))) {
+            logger.info('clean port', port);
+            await network.port.portClean(port);
+          }
+        else {
+          logger.info('clean port', parseInt(port));
+          await network.port.portClean(port);
+        }
+      }
       break;
     default:
       break;
