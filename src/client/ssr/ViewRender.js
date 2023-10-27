0b9fa9cedd18af4fcfa1658cd45ecbed30d69db7
fcoverdugo <fcoverdugoa@underpost.net>
2023-10-27 17:20:30 -0300

    update

--- src/client/ssr/ViewRender.js
+++ src/client/ssr/ViewRender.js
@@ -6,8 +6,6 @@ ViewRender = (data) => html`
       <link rel="icon" type="image/x-icon" href="${data.path !== '/' ? `${data.path}/` : '/'}favicon.ico" />
       <meta charset="UTF-8" />
       <meta name="viewport" content="initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
-      <!-- <link rel="stylesheet" href="${data.path ? `/${data.path}` : ''}/styles/app.css" />  -->
-      <!-- <script type="application/javascript" src=""></script>  -->
     </head>
     <body>
       <script async type="module" src="./${data.buildId}.js"></script>
