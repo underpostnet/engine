3fa457bfa61e087d4a0a42c60288a1f5d0f7ab80
fcoverdugo <fcoverdugoa@underpost.net>
2024-01-31 13:18:52 -0300

    update

--- src/ws/cyberia/management/cyberia.ws.bot.js
+++ src/ws/cyberia/management/cyberia.ws.bot.js
@@ -3,20 +3,23 @@ import { CyberiaWorldModel } from '../../../api/cyberia-world/cyberia-world.mode
 import { getId, random, range } from '../../../client/components/core/CommonJs.js';
 import {
   BaseElement,
+  CyberiaParams,
   WorldType,
   getRandomAvailablePosition,
+  isCollision,
 } from '../../../client/components/cyberia/CommonCyberia.js';
 import pathfinding from 'pathfinding';
 
 const CyberiaWsBotManagement = {
   element: {},
   localElementScope: {},
-  pathfinding: {},
+  pathfinding: new pathfinding.AStarFinder(),
   worlds: [],
+  biomes: [],
   instance: function (wsManagementId = '') {
     this.element[wsManagementId] = {};
     this.localElementScope[wsManagementId] = {};
-    this.pathfinding.finder = new pathfinding.AStarFinder({
+    this.pathfinding = new pathfinding.AStarFinder({
       allowDiagonal: true, // enable diagonal
       dontCrossCorners: true, // corner of a solid
       heuristic: pathfinding.Heuristic.chebyshev,
@@ -24,14 +27,6 @@ const CyberiaWsBotManagement = {
     (async () => {
       this.worlds = await CyberiaWorldModel.find();
       this.biomes = await CyberiaBiomeModel.find();
-      // const path = finder.findPath(
-      //   element.render.x,
-      //   element.render.y,
-      //   x2,
-      //   y2,
-      //   new pathfinding.Grid(botMatrixCollision)
-      // );
-
       for (const indexBot of range(0, 12)) {
         const bot = BaseElement().bot.main;
         const world = this.worlds.find((world) => world._id.toString() === bot.model.world._id);
@@ -45,6 +40,41 @@ const CyberiaWsBotManagement = {
         const id = getId(this.element[wsManagementId], 'bot-');
         this.localElementScope[wsManagementId][id] = {
           initPosition: { x, y },
+          cellRadiusMovement: 3,
+          movementPath: [],
+          movementInterval: setInterval(() => {
+            if (this.localElementScope[wsManagementId][id].movementPath.length === 0) {
+              let x;
+              let y;
+              while (
+                !x ||
+                !y ||
+                isCollision({ biomeData: biome, element: bot, x, y })
+                // ||
+                // (this.element[wsManagementId][id].x === x && this.element[wsManagementId][id].y === y)
+              ) {
+                x =
+                  this.element[wsManagementId][id].x +
+                  random(
+                    -1 * this.localElementScope[wsManagementId][id].cellRadiusMovement,
+                    this.localElementScope[wsManagementId][id].cellRadiusMovement,
+                  );
+                y =
+                  this.element[wsManagementId][id].y +
+                  random(
+                    -1 * this.localElementScope[wsManagementId][id].cellRadiusMovement,
+                    this.localElementScope[wsManagementId][id].cellRadiusMovement,
+                  );
+              }
+              this.localElementScope[wsManagementId][id].movementPath = this.pathfinding.findPath(
+                this.element[wsManagementId][id].x,
+                this.element[wsManagementId][id].y,
+                x,
+                y,
+                new pathfinding.Grid(biome.solid.map((y) => y.map((x) => (x === 0 ? 0 : 1)))),
+              );
+            }
+          }, CyberiaParams.CYBERIA_EVENT_CALLBACK_TIME),
         };
         this.element[wsManagementId][id] = bot;
       }
