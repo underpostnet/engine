48656160ef6b4802fa3f5f74f14d858b3f7223c1
fcoverdugo <fcoverdugoa@underpost.net>
2024-03-06 19:21:37 -0300

    add web crypto service

--- src/api/crypto/crypto.service.js
+++ src/api/crypto/crypto.service.js
@@ -1,6 +1,7 @@
 import { loggerFactory } from '../../server/logger.js';
 import { CryptoModel } from './crypto.model.js';
 import { endpointFactory } from '../../client/components/core/CommonJs.js';
+import crypto from 'crypto';
 
 const endpoint = endpointFactory(import.meta);
 
@@ -13,6 +14,41 @@ const select = {
 const CryptoService = {
   post: async (req, res, options) => {
     let result = {};
+    switch (req.params.id) {
+      case 'verify':
+        {
+          const signature = Buffer.from(req.body.signature, 'base64');
+
+          const publicKey = await crypto.subtle.importKey(
+            'jwk',
+            req.body.publicKey,
+            {
+              name: 'ECDSA',
+              namedCurve: 'P-384',
+              hash: 'SHA-256',
+            },
+            true,
+            ['verify'],
+          );
+
+          const isValid = await crypto.subtle.verify(
+            {
+              name: 'ECDSA',
+              hash: 'SHA-256',
+            },
+            publicKey,
+            signature,
+            new TextEncoder().encode(JSON.stringify(req.body.payload)),
+          );
+
+          result = { isValid };
+
+          logger.info('Crypto sign verify', result);
+        }
+        break;
+      default:
+        break;
+    }
     return result;
   },
   get: async (req, res, options) => {
--- src/client/components/core/Wallet.js
+++ src/client/components/core/Wallet.js
@@ -1,3 +1,4 @@
+import { CryptoService } from '../../services/crypto/crypto.service.js';
 import { BtnIcon } from './BtnIcon.js';
 import { EventsUI } from './EventsUI.js';
 
@@ -6,13 +7,52 @@ const Wallet = {
     setTimeout(async () => {
       EventsUI.onClick(`.btn-generate-keys`, async (e) => {
         e.preventDefault();
+
+        const keyPair = await window.crypto.subtle.generateKey(
+          {
+            name: 'ECDSA',
+            namedCurve: 'P-384',
+          },
+          true,
+          ['sign', 'verify'],
+        );
+        const privateKey = await window.crypto.subtle.exportKey('jwk', keyPair.privateKey);
+        const publicKey = await window.crypto.subtle.exportKey('jwk', keyPair.publicKey);
+        const payload = { data: 'test-value' };
+
+        const signature = await window.crypto.subtle.sign(
+          {
+            name: 'ECDSA',
+            hash: 'SHA-256',
+          },
+          keyPair.privateKey,
+          new TextEncoder().encode(JSON.stringify(payload)), // Encode data to Uint8Array,
+        );
+
+        const base64Signature = btoa(String.fromCharCode(...new Uint8Array(signature)));
+
+        const result = await CryptoService.post({
+          id: 'verify',
+          body: {
+            publicKey,
+            signature: base64Signature,
+            payload,
+          },
+        });
       });
     });
     return html`
-      ${await BtnIcon.Render({
-        class: 'btn-generate-keys',
-        label: 'Generate Keys',
-      })}
+      <div class="in section-mp">
+        <div class="in sub-title-modal">
+          <i class="fas fa-key"></i> Elliptic Curve Digital Signature Algorithm (ECDSA) Management
+        </div>
+      </div>
+      <div class="in">
+        ${await BtnIcon.Render({
+          class: 'btn-generate-keys',
+          label: 'Generate Keys',
+        })}
+      </div>
     `;
   },
 };
