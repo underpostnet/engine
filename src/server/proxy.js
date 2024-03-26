7693b6074202b2d2be1f160b1d7ff945028c5cc6
fcoverdugo <fcoverdugoa@underpost.net>
2024-03-26 19:42:49 -0300

    update

--- src/server/proxy.js
+++ src/server/proxy.js
@@ -121,7 +121,7 @@ const buildProxy = async () => {
     Object.keys(hosts).map((host) => {
       const { target, proxy, redirect } = hosts[host];
       if (!proxy.includes(port)) return;
-      if (redirect) return (redirects[host] = redirect);
+      if (host.split('/')[1] !== '' && redirect) return (redirects[host] = redirect);
       if (host[host.length - 1] === '/') host = host.slice(0, -1);
       if ([80, 443].includes(port)) options.router[host] = target;
       else options.router[`${host.split('/')[0]}:${port}/${host.split('/')[1]}`] = target;
