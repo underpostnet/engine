82dc6980a61f24ad402383456a602c64231cd8b0
fcoverdugo <fcoverdugoa@underpost.net>
2024-04-08 12:35:35 -0400

    update

--- bin/deploy.js
+++ bin/deploy.js
@@ -1,9 +1,11 @@
 import fs from 'fs-extra';
 
-import { shellExec } from '../src/server/process.js';
+import { shellCd, shellExec } from '../src/server/process.js';
 import { loggerFactory } from '../src/server/logger.js';
 import { Config, loadConf } from '../src/server/conf.js';
 import { buildClient } from '../src/server/client-build.js';
+import { timer } from '../src/client/components/core/CommonJs.js';
+import axios from 'axios';
 
 const logger = loggerFactory(import.meta);
 
@@ -61,6 +63,60 @@ try {
         if (filePah.split('/').pop() === 'package.json') {
           originPackage.scripts.start = JSON.parse(fs.readFileSync(filePah), 'utf8').scripts.start;
           fs.writeFileSync(filePah, JSON.stringify(originPackage, null, 4), 'utf8');
+        }
+      }
+      break;
+
+    case 'run-prod-macro':
+      {
+        const silent = true;
+        const dataDeploy = JSON.parse(fs.readFileSync(`./engine-private/deploy/${process.argv[3]}.json`, 'utf8')).map(
+          (deployId) => {
+            return {
+              deployId,
+            };
+          },
+        );
+
+        shellExec(`git pull origin master`, { silent });
+        shellCd(`engine-private`);
+        shellExec(`git pull origin master`, { silent });
+        shellCd(`..`);
+
+        for (const deploy of dataDeploy) {
+          shellExec(
+            `env-cmd -f .env.production node bin/deploy build-full-client${process.argv[4] === 'zip' ? '-zip' : ''} ${
+              deploy.deployId
+            } docs`,
+            { silent },
+          );
+        }
+
+        for (const deploy of dataDeploy) {
+          shellExec(`pm2 delete ${deploy.deployId}`, { silent });
+          shellExec(`env-cmd -f .env.production node bin/deploy run ${deploy.deployId}`, { silent });
+          await timer(3500);
+        }
+
+        for (const deploy of dataDeploy) {
+          const serverConf = JSON.parse(
+            fs.readFileSync(`./engine-private/conf/${deploy.deployId}/conf.server.json`, 'utf8'),
+          );
+          for (const host of Object.keys(serverConf))
+            for (const path of Object.keys(serverConf[host])) {
+              const result = await axios.get(`https://${host}${path}`);
+              const test = result.data.split('<title>');
+              if (test[1])
+                logger.info('Success deploy', {
+                  ...deploy,
+                  result: test.replaceAll('</title>', ''),
+                });
+              else
+                logger.error({
+                  ...deploy,
+                  result: result.data,
+                });
+            }
         }
       }
       break;
