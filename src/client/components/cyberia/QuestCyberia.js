73fc2f653e3f8a877b5673ed46bc4d8f377c29af
fcoverdugo <fcoverdugoa@underpost.net>
2024-06-10 11:18:56 -0400

    quest provider detector logic

--- src/client/components/cyberia/QuestCyberia.js
+++ src/client/components/cyberia/QuestCyberia.js
@@ -1,5 +1,49 @@
+import { loggerFactory } from '../core/Logger.js';
+import { isElementCollision } from './CommonCyberia.js';
+import { ElementsCyberia } from './ElementsCyberia.js';
+import { InteractionPanelCyberia } from './InteractionPanelCyberia.js';
+import { MatrixCyberia } from './MatrixCyberia.js';
+import { WorldCyberiaManagement } from './WorldCyberia.js';
+
+const logger = loggerFactory(import.meta);
+
+// https://github.com/underpostnet/cyberia/blob/master/modules/main/mod_quest/quest.js
+
 const QuestManagementCyberia = {
-  Load: async function () {},
+  IntervalQuestDetector: null,
+  Data: {},
+  Load: async function ({ type, id }) {
+    const radius = 3.5;
+
+    if (this.IntervalQuestDetector) clearInterval(this.IntervalQuestDetector);
+
+    if (WorldCyberiaManagement.Data[type] && WorldCyberiaManagement.Data['user']['main'])
+      this.IntervalQuestDetector = setInterval(async () => {
+        for (const instance of WorldCyberiaManagement.Data[type]['main'].model.world.instance) {
+          const botsQuest = instance.bots.filter((b) => b.behavior === 'quest-passive');
+          for (const botQuestData of botsQuest) {
+            for (const botId of Object.keys(ElementsCyberia.Data['bot'])) {
+              const displayId = ElementsCyberia.getCurrentSkinDisplayId({ type: 'bot', id: botId });
+              if (
+                botQuestData.displayIds.includes(displayId) &&
+                isElementCollision({
+                  A: {
+                    dim: ElementsCyberia.Data['bot'][botId].dim * radius,
+                    x: ElementsCyberia.Data['bot'][botId].x - (ElementsCyberia.Data['bot'][botId].dim * radius) / 2,
+                    y: ElementsCyberia.Data['bot'][botId].y - (ElementsCyberia.Data['bot'][botId].dim * radius) / 2,
+                  },
+                  B: ElementsCyberia.Data['user']['main'],
+                  dimPaintByCell: MatrixCyberia.Data.dimPaintByCell,
+                })
+              ) {
+                logger.warn('quest provider detector', { type: 'bot', id: botId });
+                return await InteractionPanelCyberia.PanelRender.element({ type: 'bot', id: botId });
+              }
+            }
+          }
+        }
+      }, 500);
+  },
 };
 
 const QuestCyberia = {
