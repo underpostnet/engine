efeb5f603cdcc5c69deb2e7d2d4f926b2f43cd54
fcoverdugo <fcoverdugoa@underpost.net>
2024-08-06 19:54:09 -0400

    update

--- src/client/components/dogmadual/MenuDogmadual.js
+++ src/client/components/dogmadual/MenuDogmadual.js
@@ -34,7 +34,9 @@ const MenuDogmadual = {
               text: html`${Translate.Render('home')}`,
             }),
             // style: 'display: none',
-            attrs: `data-id="0"`,
+            attrs: `data-id="home"`,
+            tabHref: `${getProxyPath()}`,
+            handleContainerClass: 'handle-btn-container',
           })}
           ${await BtnIcon.Render({
             class: 'in wfa main-btn-menu main-btn-log-in',
@@ -42,7 +44,9 @@ const MenuDogmadual = {
               icon: html`<i class="fas fa-sign-in-alt"></i>`,
               text: html`${Translate.Render('log-in')}`,
             }),
-            attrs: `data-id="1"`,
+            attrs: `data-id="log-in"`,
+            tabHref: `${getProxyPath()}log-in`,
+            handleContainerClass: 'handle-btn-container',
           })}
           ${await BtnIcon.Render({
             class: 'in wfa main-btn-menu main-btn-sign-up',
@@ -50,7 +54,9 @@ const MenuDogmadual = {
               icon: html`<i class="fas fa-user-plus"></i>`,
               text: html`${Translate.Render('sign-up')}`,
             }),
-            attrs: `data-id="2"`,
+            attrs: `data-id="sign-up"`,
+            tabHref: `${getProxyPath()}sign-up`,
+            handleContainerClass: 'handle-btn-container',
           })}
           ${await BtnIcon.Render({
             class: 'in wfa main-btn-menu main-btn-log-out',
@@ -58,8 +64,10 @@ const MenuDogmadual = {
               icon: html`<i class="fas fa-sign-out-alt"></i>`,
               text: html`${Translate.Render('log-out')}`,
             }),
-            attrs: `data-id="3"`,
+            attrs: `data-id="log-out"`,
             style: 'display: none',
+            tabHref: `${getProxyPath()}log-out`,
+            handleContainerClass: 'handle-btn-container',
           })}
           ${await BtnIcon.Render({
             class: 'in wfa main-btn-menu main-btn-account',
@@ -68,7 +76,9 @@ const MenuDogmadual = {
               text: html`${Translate.Render('account')}`,
             }),
             style: 'display: none',
-            attrs: `data-id="4"`,
+            attrs: `data-id="account"`,
+            tabHref: `${getProxyPath()}account`,
+            handleContainerClass: 'handle-btn-container',
           })}
         </div>
       `,
@@ -90,55 +100,54 @@ const MenuDogmadual = {
       heightBottomBar,
     });
 
-    this.Data[id].sortable = Modal.mobileModal()
-      ? null
-      : new Sortable(s(`.menu-btn-container`), {
-          animation: 150,
-          group: `menu-sortable`,
-          forceFallback: true,
-          fallbackOnBody: true,
-          store: {
-            /**
-             * Get the order of elements. Called once during initialization.
-             * @param   {Sortable}  sortable
-             * @returns {Array}
-             */
-            get: function (sortable) {
-              const order = localStorage.getItem(sortable.options.group.name);
-              return order ? order.split('|') : [];
-            },
+    this.Data[id].sortable = new Sortable(s(`.menu-btn-container`), {
+      animation: 150,
+      group: `menu-sortable`,
+      forceFallback: true,
+      fallbackOnBody: true,
+      handle: '.handle-btn-container',
+      store: {
+        /**
+         * Get the order of elements. Called once during initialization.
+         * @param   {Sortable}  sortable
+         * @returns {Array}
+         */
+        get: function (sortable) {
+          const order = localStorage.getItem(sortable.options.group.name);
+          return order ? order.split('|') : [];
+        },
 
-            /**
-             * Save the order of elements. Called onEnd (when the item is dropped).
-             * @param {Sortable}  sortable
-             */
-            set: function (sortable) {
-              const order = sortable.toArray();
-              localStorage.setItem(sortable.options.group.name, order.join('|'));
-            },
-          },
-          // chosenClass: 'css-class',
-          // ghostClass: 'css-class',
-          // Element dragging ended
-          onEnd: function (/**Event*/ evt) {
-            // console.log('Sortable onEnd', evt);
-            // console.log('evt.oldIndex', evt.oldIndex);
-            // console.log('evt.newIndex', evt.newIndex);
-            const slotId = Array.from(evt.item.classList).pop();
-            // console.log('slotId', slotId);
-            if (evt.oldIndex === evt.newIndex) s(`.${slotId}`).click();
+        /**
+         * Save the order of elements. Called onEnd (when the item is dropped).
+         * @param {Sortable}  sortable
+         */
+        set: function (sortable) {
+          const order = sortable.toArray();
+          localStorage.setItem(sortable.options.group.name, order.join('|'));
+        },
+      },
+      // chosenClass: 'css-class',
+      // ghostClass: 'css-class',
+      // Element dragging ended
+      onEnd: function (/**Event*/ evt) {
+        // console.log('Sortable onEnd', evt);
+        // console.log('evt.oldIndex', evt.oldIndex);
+        // console.log('evt.newIndex', evt.newIndex);
+        const slotId = Array.from(evt.item.classList).pop();
+        // console.log('slotId', slotId);
+        if (evt.oldIndex === evt.newIndex) s(`.${slotId}`).click();
 
-            // var itemEl = evt.item; // dragged HTMLElement
-            // evt.to; // target list
-            // evt.from; // previous list
-            // evt.oldIndex; // element's old index within old parent
-            // evt.newIndex; // element's new index within new parent
-            // evt.oldDraggableIndex; // element's old index within old parent, only counting draggable elements
-            // evt.newDraggableIndex; // element's new index within new parent, only counting draggable elements
-            // evt.clone; // the clone element
-            // evt.pullMode; // when item is in another sortable: `"clone"` if cloning, `true` if moving
-          },
-        });
+        // var itemEl = evt.item; // dragged HTMLElement
+        // evt.to; // target list
+        // evt.from; // previous list
+        // evt.oldIndex; // element's old index within old parent
+        // evt.newIndex; // element's new index within new parent
+        // evt.oldDraggableIndex; // element's old index within old parent, only counting draggable elements
+        // evt.newDraggableIndex; // element's new index within new parent, only counting draggable elements
+        // evt.clone; // the clone element
+        // evt.pullMode; // when item is in another sortable: `"clone"` if cloning, `true` if moving
+      },
+    });
 
     EventsUI.onClick(`.main-btn-sign-up`, async () => {
       const { barConfig } = await Themes[Css.currentTheme]();
