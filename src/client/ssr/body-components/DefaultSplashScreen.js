c848954c0e17f3724ba34c09834006dc72548cc4
fcoverdugo <fcoverdugoa@underpost.net>
2024-08-19 17:53:37 -0400

    update

--- src/client/ssr/body-components/DefaultSplashScreen.js
+++ src/client/ssr/body-components/DefaultSplashScreen.js
@@ -33,8 +33,7 @@ SrrComponent = () => html` <div class="ssr-background" style="opacity: 1">
     .ssr-background {
       position: absolute;
       display: block;
-      background: #ececec;
-      color: #5f5f5f;
+      color: #f7f7f7;
       width: 100%;
       height: 100%;
       top: 0px;
@@ -74,6 +73,87 @@ SrrComponent = () => html` <div class="ssr-background" style="opacity: 1">
       -o-user-select: none;
     }
   </style>
+  <style>
+    .ssr-background {
+      margin: auto;
+      font-family: -apple-system, BlinkMacSystemFont, sans-serif;
+      overflow: auto;
+      background: linear-gradient(
+        315deg,
+        rgba(101, 0, 94, 1) 3%,
+        rgba(60, 132, 206, 1) 38%,
+        rgba(48, 238, 226, 1) 68%,
+        rgba(255, 25, 25, 1) 98%
+      );
+      animation: gradient 15s ease infinite;
+      background-size: 400% 400%;
+      background-attachment: fixed;
+    }
+
+    @keyframes gradient {
+      0% {
+        background-position: 0% 0%;
+      }
+      50% {
+        background-position: 100% 100%;
+      }
+      100% {
+        background-position: 0% 0%;
+      }
+    }
+
+    .wave {
+      background: rgb(255 255 255 / 25%);
+      border-radius: 1000% 1000% 0 0;
+      position: fixed;
+      width: 400%;
+      height: 100%;
+      animation: wave 10s -3s linear infinite;
+      transform: translate3d(0, 0, 0);
+      opacity: 0.8;
+      left: 0;
+      top: 20%;
+    }
+
+    .wave:nth-of-type(2) {
+      animation: wave 18s linear reverse infinite;
+      opacity: 0.8;
+      top: 50%;
+    }
+
+    .wave:nth-of-type(3) {
+      animation: wave 20s -1s reverse infinite;
+      opacity: 0.9;
+      top: 70%;
+    }
+
+    @keyframes wave {
+      2% {
+        transform: translateX(1);
+      }
+
+      25% {
+        transform: translateX(-25%);
+      }
+
+      50% {
+        transform: translateX(-50%);
+      }
+
+      75% {
+        transform: translateX(-25%);
+      }
+
+      100% {
+        transform: translateX(1);
+      }
+    }
+  </style>
+  <div>
+    <div class="wave"></div>
+    <div class="wave"></div>
+    <div class="wave"></div>
+  </div>
   <div class="ssr-center">
     <!-- <img class="ssr-logo" /> -->
     <div class="lds-dual-ring"></div>
