23cdf3aa8969bc2772604985cb349a42fb9a226b
fcoverdugo <fcoverdugoa@underpost.net>
2024-08-21 18:17:59 -0400

    update

--- src/client/ssr/head-components/DefaultScripts.js
+++ src/client/ssr/head-components/DefaultScripts.js
@@ -1,3 +1,4 @@
 SrrComponent = ({ ssrPath }) => html`
   <script type="text/javascript" src="${ssrPath}dist/validator/validator.min.js"></script>
+  <script type="text/javascript" src="${ssrPath}dist/@hcaptcha/index.min.js"></script>
 `;
