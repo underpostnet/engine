9954af31e6aec1ec609445008cc8ec758e36edf6
fcoverdugo <fcoverdugoa@underpost.net>
2024-08-30 16:48:10 -0400

    update

--- src/server/client-build-live.js
+++ src/server/client-build-live.js
@@ -15,28 +15,30 @@ const clientLiveBuild = async () => {
     const updates = JSON.parse(fs.readFileSync(`./tmp/client.build.json`, 'utf8'));
     const liveClientBuildPaths = [];
     for (let srcPath of updates) {
+      const srcBuildPath = `./src${srcPath.split('src')[1].replace(/\\/g, '/')}`;
       if (
         srcPath.split('src')[1].startsWith(`\\client\\components`) ||
         srcPath.split('src')[1].startsWith(`\\client\\services`)
       ) {
-        const buildPath = `./public/${baseHost}/${srcPath.split('src')[1].slice(8)}`.replace(/\\/g, '/');
-        liveClientBuildPaths.push(buildPath);
+        const publicBuildPath = `./public/${baseHost}/${srcPath.split('src')[1].slice(8)}`.replace(/\\/g, '/');
+        liveClientBuildPaths.push({ srcBuildPath, publicBuildPath });
       } else if (srcPath.split('src')[1].startsWith(`\\client\\sw`)) {
-        const buildPath = `./public/${baseHost}/sw.js`;
-        liveClientBuildPaths.push(buildPath);
+        const publicBuildPath = `./public/${baseHost}/sw.js`;
+        liveClientBuildPaths.push({ srcBuildPath, publicBuildPath });
       } else if (srcPath.split('src')[1].startsWith(`\\client`) && srcPath.slice(-9) === '.index.js') {
         for (const view of Config.default.client[clientId].views) {
-          const buildPath = `./public/${baseHost}${view.path === '/' ? '' : view.path}/${clientId}.index.js`;
-          liveClientBuildPaths.push(buildPath);
+          const publicBuildPath = `./public/${baseHost}${view.path === '/' ? '' : view.path}/${clientId}.index.js`;
+          liveClientBuildPaths.push({ srcBuildPath, publicBuildPath });
         }
       } else if (srcPath.split('src')[1].startsWith(`\\client\\ssr`)) {
         for (const view of Config.default.client[clientId].views) {
-          const buildPath = `./public/${baseHost}${view.path === '/' ? '' : view.path}/index.html`;
-          liveClientBuildPaths.push(buildPath);
+          const publicBuildPath = `./public/${baseHost}${view.path === '/' ? '' : view.path}/index.html`;
+          liveClientBuildPaths.push({ srcBuildPath, publicBuildPath });
         }
       }
     }
     logger.info('liveClientBuildPaths', liveClientBuildPaths);
+    //  ->
     // await buildClient({ liveClientBuildPaths });
     fs.removeSync(`./tmp/client.build.json`);
   }
