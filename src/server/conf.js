9862bbeb9581afd2559dca2c0d47a424c248dc7e
fcoverdugo <fcoverdugoa@underpost.net>
2024-08-30 20:38:52 -0400

    update

--- src/server/conf.js
+++ src/server/conf.js
@@ -637,6 +637,7 @@ const buildProxyRouter = () => {
   const proxyRouter = {};
   for (const host of Object.keys(confServer)) {
     for (const path of Object.keys(confServer[host])) {
+      if (confServer[host][path].singleReplica) continue;
       confServer[host][path].port = newInstance(currentPort);
       for (const port of confServer[host][path].proxy) {
         if (!(port in proxyRouter)) proxyRouter[port] = {};
