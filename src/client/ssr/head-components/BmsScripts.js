a46a7c2dfe670616132231a73a1f04344e43ab18
fcoverdugo <fcoverdugoa@underpost.net>
2024-09-03 09:05:59 -0400

    update

--- src/client/ssr/head-components/BmsScripts.js
+++ src/client/ssr/head-components/BmsScripts.js
@@ -1,6 +1,8 @@
 SrrComponent = ({ ssrPath }) => html`
   <script type="text/javascript" src="${ssrPath}dist/validator/validator.min.js"></script>
   <script type="text/javascript" src="${ssrPath}dist/tinymce/tinymce.min.js"></script>
-  <script src="${ssrPath}dist/d3/d3.min.js"></script>
-  <script src="${ssrPath}dist/peerjs/peerjs.min.js"></script>
+  <script type="text/javascript" src="${ssrPath}dist/d3/d3.min.js"></script>
+  <script type="text/javascript" src="${ssrPath}dist/peerjs/peerjs.min.js"></script>
+  <script type="text/javascript" src="${ssrPath}dist/@fullcalendar/core/index.global.min.js"></script>
+  <script type="text/javascript" src="${ssrPath}dist/@fullcalendar/daygrid/index.global.min.js"></script>
 `;
