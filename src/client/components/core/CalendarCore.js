0d140566139bd64c3580d169a0484f380a827982
fcoverdugo <fcoverdugoa@underpost.net>
2024-09-02 21:38:16 -0400

    add fullcalendar

--- src/client/components/core/CalendarCore.js
+++ src/client/components/core/CalendarCore.js
@@ -2,8 +2,17 @@ import { s } from './VanillaJs.js';
 
 const CalendarCore = {
   Render: async function () {
-    setTimeout(() => {});
-    return html`calendar`;
+    setTimeout(() => {
+      const calendarEl = s('#calendar');
+      const calendar = new FullCalendar.Calendar(calendarEl, {
+        // plugins: [dayGridPlugin],
+        initialView: 'dayGridMonth',
+        events: [{ title: 'Meeting', start: new Date() }],
+      });
+
+      calendar.render();
+    });
+    return html` <div id="calendar"></div>`;
   },
 };
 
