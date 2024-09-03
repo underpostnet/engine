40c207f13f3e6d7ab71fc4cc203ce6ff02101bb5
fcoverdugo <fcoverdugoa@underpost.net>
2024-09-02 20:15:43 -0400

    remove toast-ui

--- src/client/components/core/CalendarCore.js
+++ src/client/components/core/CalendarCore.js
@@ -2,42 +2,8 @@ import { s } from './VanillaJs.js';
 
 const CalendarCore = {
   Render: async function () {
-    setTimeout(() => {
-      const container = s('#calendar');
-
-      /** @type {import('@toast-ui/calendar').Options} */
-      const options = {
-        defaultView: 'week',
-        timezone: {
-          zones: [
-            {
-              timezoneName: 'Asia/Seoul',
-              displayLabel: 'Seoul',
-            },
-            {
-              timezoneName: 'Europe/London',
-              displayLabel: 'London',
-            },
-          ],
-        },
-        calendars: [
-          {
-            id: 'cal1',
-            name: 'Personal',
-            backgroundColor: '#03bd9e',
-          },
-          {
-            id: 'cal2',
-            name: 'Work',
-            backgroundColor: '#00a9ff',
-          },
-        ],
-      };
-      /** @type {import('@toast-ui/calendar').default} */
-      const Calendar = tui.Calendar;
-      this.instance = new Calendar(container, options);
-    });
-    return html`<div id="calendar"></div>`;
+    setTimeout(() => {});
+    return html`calendar`;
   },
 };
 
--- src/client/ssr/head-components/BmsScripts.js
+++ src/client/ssr/head-components/BmsScripts.js
@@ -1,8 +1,6 @@
 SrrComponent = ({ ssrPath }) => html`
   <script type="text/javascript" src="${ssrPath}dist/validator/validator.min.js"></script>
   <script type="text/javascript" src="${ssrPath}dist/tinymce/tinymce.min.js"></script>
-  <link rel="stylesheet" href="${ssrPath}dist/toast-ui/toastui-calendar.min.css" />
-  <script src="${ssrPath}dist/toast-ui/toastui-calendar.ie11.min.js"></script>
   <script src="${ssrPath}dist/d3/d3.min.js"></script>
   <script src="${ssrPath}dist/peerjs/peerjs.min.js"></script>
 `;
