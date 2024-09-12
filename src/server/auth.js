ed1453a0b5e6faacfce7d411d55d44c7aa4e7d3d
fcoverdugo <fcoverdugoa@underpost.net>
2024-09-12 09:36:19 -0300

    update

--- src/server/auth.js
+++ src/server/auth.js
@@ -15,19 +15,11 @@ dotenv.config();
 
 const logger = loggerFactory(import.meta);
 
-// password
-
-// larger numbers mean better security
+/* The `const config` object is defining parameters related to the hashing process used for password
+security. Here's a breakdown of each property in the `config` object: */
 const config = {
-  // size of the generated hash
   hashBytes: 32,
-  // larger salt means hashed passwords are more resistant to rainbow table, but
-  // you get diminishing returns pretty fast
   saltBytes: 16,
-  // more iterations means an attacker has to take longer to brute force an
-  // individual password, so larger is better. however, larger also means longer
-  // to hash the password. tune so that hashing the password takes about a
-  // second
   iterations: 872791,
   digest: 'sha512',
 };
