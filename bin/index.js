e6b101d92184b0e83fa3177c5766e72e689bd0ea
fcoverdugo <fcoverdugoa@underpost.net>
2024-09-13 16:13:38 -0300

    update

--- bin/index.js
+++ bin/index.js
@@ -3,9 +3,21 @@
 import { spawn } from 'node:child_process';
 import { loggerFactory } from '../src/server/logger.js';
 import dotenv from 'dotenv';
+import { shellCd, shellExec } from '../src/server/process.js';
 
 dotenv.config();
 
 const logger = loggerFactory(import.meta);
 
 await logger.setUpInfo();
+
+switch (process.argv[3]) {
+  case 'run-dev':
+    // underpost run-dev
+    shellCd(`./node_modules/underpost-pwa-api-rest-template`);
+    shellExec(`npm run dev`);
+    break;
+
+  default:
+    break;
+}
