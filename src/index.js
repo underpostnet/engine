adbd8901ee02301533a80401ed702798005243f4
fcoverdugo <fcoverdugoa@underpost.net>
2024-09-19 12:32:44 -0300

    add Underpost index npm package doc

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
-import { loggerFactory } from './server/logger.js';
+/**
+ * Underpost index npm package
+ * @module src/client/index.js
+ * @namespace Underpost
+ */
+
+import { loggerFactory, setUpInfo } from './server/logger.js';
 
 const logger = loggerFactory(import.meta);
 
 const underpost = {
-  setUpInfo: logger.setUpInfo,
+  /**
+   * Logs information about the current process environment to the console.
+   *
+   * This function is used to log details about
+   * the execution context, such as command-line arguments,
+   * environment variables, the process's administrative privileges,
+   * and the maximum available heap space size.
+   *
+   * @memberof Underpost
+   */
+  setUpInfo: async () => await setUpInfo(logger),
 };
 
 const up = underpost;
