fe59d0dcdebe6007020b7c4ec882df2755cb0300
fcoverdugo <fcoverdugoa@underpost.net>
2024-09-26 20:02:32 -0300

    update cyberia scripts

--- src/client/ssr/head-components/CyberiaPortalScripts.js
+++ src/client/ssr/head-components/CyberiaPortalScripts.js
@@ -1,3 +1,5 @@
 SrrComponent = ({ ssrPath }) => html`
   <script type="text/javascript" src="${ssrPath}dist/validator/validator.min.js"></script>
+  <script type="text/javascript" src="${ssrPath}dist/easymde/easymde.min.js"></script>
+  <link rel="stylesheet" href="${ssrPath}dist/easymde/easymde.min.css" />
 `;
