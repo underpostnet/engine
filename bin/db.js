581b08effbd7238eb9fbfd15992f359eecd12b21
fcoverdugo <fcoverdugoa@underpost.net>
2024-10-07 13:14:21 -0400

    fix mysqldump export

--- bin/db.js
+++ bin/db.js
@@ -62,20 +62,7 @@ try {
           await MariaDB.query({ user, password, query: `SELECT ${arg0} FROM ${name}.${arg1}` });
           break;
         case 'export':
-          // cmd = `mysqldump --column-statistics=0
-          const mysqlPasswordPath = `./tmp/mysqlpassword.cnf`;
-
-          fs.writeFileSync(
-            mysqlPasswordPath,
-            `[mysqldump]
-# The following password will be sent to mysqldump 
-password="${password}"`,
-            'utf8',
-          );
-
-          cmd = `mysqldump --defaults-extra-file=${mysqlPasswordPath} -u ${user} -p ${name} > ${
-            arg0 ? `${arg0}/${name}.sql` : backupPath
-          }`;
+          cmd = `mysqldump -u ${user} -p${password} ${name} > ${arg0 ? `${arg0}/${name}.sql` : backupPath}`;
           shellExec(cmd);
           break;
         case 'import':
