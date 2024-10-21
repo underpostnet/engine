5ed1afd2f649c9568600b8589e6499fe1d3a2415
fcoverdugoa <fcoverdugoa@underpost.net>
2024-10-21 20:59:41 +0500

    add base display lore splash screen cyberia

--- src/client/ssr/body-components/CyberiaSplashScreenLore.js
+++ src/client/ssr/body-components/CyberiaSplashScreenLore.js
@@ -1,3 +1,87 @@
+const getLang = () => navigator.language || navigator.userLanguage;
+const s = (el) => document.querySelector(el);
+const htmls = (el, html) => (s(el).innerHTML = html);
+const typeWriter = async function ({ id, html, seconds, endHideBlink, container }) {
+  if (!seconds) seconds = 2;
+  return new Promise((resolve) => {
+    // https://developer.mozilla.org/en-US/docs/Web/CSS/animation-timing-function
+    // https://www.w3schools.com/cssref/css3_pr_animation-fill-mode.php
+    const typingAnimationTransitionStyle = [`1s linear`, `${seconds}s steps(30, end)`, `1s forwards`];
+    const render = html`
+      <style class="style-${id}">
+        .tw-${id}-typed-out {
+          overflow: hidden;
+          border-right: 0.15em solid orange;
+          white-space: nowrap;
+          animation: typing-${id} ${typingAnimationTransitionStyle[1]}, blink-caret-${id} 0.5s step-end infinite;
+          animation-fill-mode: forwards;
+          width: 0;
+        }
+      </style>
+      <style>
+        .tw-${id}-container {
+        }
+        @keyframes typing-${id} {
+          from {
+            width: 0;
+          }
+          to {
+            width: 100%;
+          }
+        }
+
+        @keyframes blink-caret-${id} {
+          from,
+          to {
+            border-color: transparent;
+          }
+          50% {
+            border-color: orange;
+          }
+        }
+      </style>
+      <div class="inl tw-${id}-container">
+        <div class="tw-${id}-typed-out">${html}</div>
+      </div>
+    `;
+    htmls(`.${container}`, render);
+    setTimeout(() => {
+      if (endHideBlink && s(`.style-${id}`)) s(`.style-${id}`).remove();
+      resolve(render);
+    }, seconds * 1000);
+  });
+};
+
+const LoreScreen = async () => {
+  const translate = {
+    en: html` <br />One Conflict <br /><br />
+      <div class="ssr-lore-text">
+        In a galaxy scarred by catastrophe, three factions vie for dominance. <br />
+        <br /><strong class="ssr-secondary-color">The Colonists</strong>, descendants of Earth's pioneers, seek to
+        expand their dominion across the stars. <br /><br /><strong class="ssr-secondary-color">The Renegades</strong>,
+        mutants born from the ashes of disaster, fight for survival and retribution. <br /><br />
+        <strong class="ssr-secondary-color">The Synthetics</strong>, a fusion of flesh and machine, strive for a future
+        beyond human limitations
+      </div>
+      <br />
+      <br />`,
+    es: html` <br />
+      Un Conflicto <br /><br />
+      <div class="ssr-lore-text">
+        En una galaxia marcada por la catástrofe, tres facciones compiten por el dominio. <br />
+        <br /><strong class="ssr-secondary-color">Los colonos</strong>, descendientes de los pioneros de la Tierra,
+        buscan expandir su dominio a través de las estrellas. <br />
+        <br /><strong class="ssr-secondary-color">Los renegados</strong>, mutantes nacidos de las cenizas del desastre,
+        luchan por la supervivencia y la venganza.
+        <br />
+        <br />
+        <strong class="ssr-secondary-color">Los sintéticos</strong>, una fusión de carne y máquina, luchan por un futuro
+        más allá de las limitaciones humanas
+      </div>`,
+  };
+  await typeWriter({ id: 'ssr-lore', html: translate[getLang()] || translate.en, container: 'ssr-lore-container' });
+};
+
 SrrComponent = ({ host, path }) => html`
   <div class="ssr-background" style="opacity: 1">
     <style>
@@ -78,7 +162,23 @@ SrrComponent = ({ host, path }) => html`
         display: block;
         bottom: 0px;
       }
+      .ssr-lore-container {
+        display: block;
+        position: relative;
+        margin: 5px;
+      }
+      .ssr-lore-text {
+        color: white;
+        font-size: 13px;
+        font-weight: normal;
+      }
+      .ssr-secondary-color {
+        color: #ffcc00;
+      }
     </style>
+
+    <div class="ssr-lore-container"></div>
+
     <div class="ssr-loading-container">
       <div class="ssr-loading-bar"><div class="ssr-loading-bar-block ssr-blink-bar"></div></div>
       <div class="ssr-loading-info">
@@ -86,5 +186,16 @@ SrrComponent = ({ host, path }) => html`
         ...${`${host}${path}`.slice(-30)}
       </div>
     </div>
+    <script>
+      {
+        const s = ${s};
+        const htmls = ${htmls};
+        const typeWriter = ${typeWriter};
+        const getLang = ${getLang};
+        const LoreScreen = ${LoreScreen};
+
+        LoreScreen();
+      }
+    </script>
   </div>
 `;
