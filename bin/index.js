41b96b63e48759a3bb8ab54c0573536db8ef4143
fcoverdugoa <fcoverdugoa@underpost.net>
2024-11-14 21:45:13 +0500

    update to version 2.7.9

--- bin/index.js
+++ bin/index.js
@@ -19,7 +19,7 @@ const globalBinFolder = `${shellExec(`npm root -g`, {
 
 const program = new Command();
 
-const version = '2.7.83';
+const version = '2.7.9';
 
 program.name('underpost').description(`underpost.net ci/cd cli ${version}`).version(version);
 
