2841927114d82eae06fcfd5967d10b2c15edc4f3
fcoverdugoa <fcoverdugoa@underpost.net>
2024-11-15 20:00:06 +0500

    add dns comments

--- src/server/dns.js
+++ src/server/dns.js
@@ -22,11 +22,14 @@ const Dns = {
   repoUrl: `https://${process.env.GITHUB_TOKEN}@github.com/${process.env.GITHUB_USERNAME}/${process.env.GITHUB_DNS_REPO}.git`,
   callback: () => null,
   InitIpDaemon: async function ({ deployId }) {
-    // WAN | NAT-VPS | LAN
+    // NAT-VPS modem/router device configuration:
+    // LAN --> [NAT-VPS] --> WAN
     // enabled DMZ Host to proxy IP 80-443 (79-444) sometimes router block first port
-    // LAN server or device's local servers port ->  3000-3100 (2999-3101)
-    // DNS Records: [ANAME](Address Dynamic) -> [A](ipv4) host | [AAAA](ipv6) host -> [ip]
+    // disabled local red DHCP
+    // verify inet ip proxy server address
     // DHCP (Dynamic Host Configuration Protocol) LAN reserver IP -> MAC ID
+    // LAN server or device's local servers port ->  3000-3100 (2999-3101)
+    // DNS Records: [ANAME](Address Dynamic) -> [A](ipv4) host | [AAAA](ipv6) host -> [public-ip]
     // Forward the router's TCP/UDP ports to the LAN device's IP address
 
     const privateCronConfPath = `./engine-private/conf/${deployId}/conf.cron.json`;
