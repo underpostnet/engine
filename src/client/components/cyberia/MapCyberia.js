2fb990b49d0e909948cca08df0dc87b31f87bbfc
fcoverdugoa <fcoverdugoa@underpost.net>
2024-11-21 18:19:07 -0300

    cyberia universe map advance

--- src/client/components/cyberia/MapCyberia.js
+++ src/client/components/cyberia/MapCyberia.js
@@ -3,7 +3,7 @@ import { getProxyPath } from '../core/VanillaJs.js';
 
 const MapCyberia = {
   Render: async function () {
-    const renderFactionMapSymbol = ({ name, color, imgPath, left, top }) => {
+    const renderZoneLegendInfo = ({ name, color, imgPath, left, top, style }) => {
       return html`
         <div
           class="abs map-faction-symbol-container"
@@ -12,11 +12,12 @@ const MapCyberia = {
               left,
               top,
               border: `2px solid ${color}`,
+              ...(style ? style : {}),
             },
           })}"
         >
-          <img class="inl map-faction-symbol-img" src="${getProxyPath()}${imgPath}" />
-          <span class="inl map-text-shadow-${color}" style="top: -10px"> ${name}</span>
+          ${imgPath ? html`<img class="inl map-faction-symbol-img" src="${getProxyPath()}${imgPath}" />` : ''}
+          <span class="inl map-text-shadow-${color}" ${imgPath ? html`style="top: -10px"` : ''}> ${name}</span>
         </div>
       `;
     };
@@ -44,67 +45,113 @@ const MapCyberia = {
           width: 35px;
           height: 35px;
         }
+        .blink-current-universe-position-container {
+          width: 35px;
+          height: 35px;
+        }
+        .blink-current-universe-position {
+          animation: blink-current-universe-position 1s infinite;
+          transition: 1s;
+        }
+        @keyframes blink-current-universe-position {
+          0% {
+            width: 35px;
+            height: 35px;
+          }
+          50% {
+            width: 25px;
+            height: 25px;
+          }
+          100% {
+            width: 35px;
+            height: 35px;
+          }
+        }
       </style>
       ${borderChar(1, 'red', ['.map-text-shadow-red'])} ${borderChar(1, 'yellow', ['.map-text-shadow-yellow'])}
-      ${borderChar(1, 'blue', ['.map-text-shadow-blue'])}
+      ${borderChar(1, 'blue', ['.map-text-shadow-blue'])} ${borderChar(1, 'white', ['.map-text-shadow-white'])}
       <div class="in map-modal-universe-background-image">
         <img class="in map-modal-universe-background-image" src="${getProxyPath()}assets/lore/universe/_0.jpeg" />
-        ${renderFactionMapSymbol({
+        ${renderZoneLegendInfo({
           imgPath: 'assets/lore/macro-factions/zenith-empire.jpeg',
           name: 'Zenith Empire',
           color: 'red',
           left: '5%',
           top: '38%',
         })}
-        ${renderFactionMapSymbol({
+        ${renderZoneLegendInfo({
           imgPath: 'assets/lore/macro-factions/zenith-empire.jpeg',
           name: 'Zenith Empire',
           color: 'red',
           left: '56%',
           top: '22%',
         })}
-        ${renderFactionMapSymbol({
+        ${renderZoneLegendInfo({
           imgPath: 'assets/lore/macro-factions/atlas-confederation.jpeg',
           name: 'Atlas Confederation',
           color: 'yellow',
           left: '13%',
           top: '64%',
         })}
-        ${renderFactionMapSymbol({
+        ${renderZoneLegendInfo({
           imgPath: 'assets/lore/macro-factions/atlas-confederation.jpeg',
           name: 'Atlas Confederation',
           color: 'yellow',
           left: '14%',
           top: '11%',
         })}
-        ${renderFactionMapSymbol({
+        ${renderZoneLegendInfo({
           imgPath: 'assets/lore/macro-factions/nova-republic.jpeg',
           name: 'Nova Republic',
           color: 'blue',
           left: '48%',
           top: '6%',
         })}
-        ${renderFactionMapSymbol({
+        ${renderZoneLegendInfo({
           imgPath: 'assets/lore/macro-factions/zenith-empire.jpeg',
           name: 'Zenith Empire',
           color: 'red',
           left: '42%',
           top: '76%',
         })}
-        ${renderFactionMapSymbol({
+        ${renderZoneLegendInfo({
           imgPath: 'assets/lore/macro-factions/nova-republic.jpeg',
           name: 'Nova Republic',
           color: 'blue',
           left: '67%',
           top: '63%',
         })}
-        ${renderFactionMapSymbol({
+        ${renderZoneLegendInfo({
           imgPath: 'assets/lore/macro-factions/atlas-confederation.jpeg',
           name: 'Atlas Confederation',
           color: 'yellow',
           left: '37%',
           top: '51%',
         })}
+        ${renderZoneLegendInfo({
+          name: 'Seed City',
+          color: 'white',
+          left: '50%',
+          top: '61%',
+          style: {
+            'min-width': '0px',
+            'min-height': '0px',
+          },
+        })}
+        <div
+          class="abs blink-current-universe-position-container"
+          style="${renderCssAttr({
+            style: {
+              top: '61%',
+              left: '45%',
+            },
+          })}"
+        >
+          <img
+            src="${getProxyPath()}/assets/ui-icons/fullscreen.png"
+            class="abs center blink-current-universe-position"
+          />
+        </div>
       </div>
     `;
   },
