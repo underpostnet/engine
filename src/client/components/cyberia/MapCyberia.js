b5f4741a2803318a57abd326381b0f582f70e721
fcoverdugoa <fcoverdugoa@underpost.net>
2024-11-21 16:49:26 -0300

    cyberia universe map advance

--- src/client/components/cyberia/MapCyberia.js
+++ src/client/components/cyberia/MapCyberia.js
@@ -16,7 +16,7 @@ const MapCyberia = {
           })}"
         >
           <img class="inl map-faction-symbol-img" src="${getProxyPath()}${imgPath}" />
-          <span class="inl" style="top: -10px"> ${name}</span>
+          <span class="inl map-text-shadow-${color}" style="top: -10px"> ${name}</span>
         </div>
       `;
     };
@@ -45,7 +45,8 @@ const MapCyberia = {
           height: 35px;
         }
       </style>
-      ${borderChar(1, 'red', ['.map-faction-symbol-container'])}
+      ${borderChar(1, 'red', ['.map-text-shadow-red'])} ${borderChar(1, 'yellow', ['.map-text-shadow-yellow'])}
+      ${borderChar(1, 'blue', ['.map-text-shadow-blue'])}
       <div class="in map-modal-universe-background-image">
         <img class="in map-modal-universe-background-image" src="${getProxyPath()}assets/lore/universe/_0.jpeg" />
         ${renderFactionMapSymbol({
@@ -61,6 +62,48 @@ const MapCyberia = {
           color: 'red',
           left: '56%',
           top: '22%',
+        })}
+        ${renderFactionMapSymbol({
+          imgPath: 'assets/lore/macro-factions/atlas-confederation.jpeg',
+          name: 'Atlas Confederation',
+          color: 'yellow',
+          left: '13%',
+          top: '64%',
+        })}
+        ${renderFactionMapSymbol({
+          imgPath: 'assets/lore/macro-factions/atlas-confederation.jpeg',
+          name: 'Atlas Confederation',
+          color: 'yellow',
+          left: '14%',
+          top: '11%',
+        })}
+        ${renderFactionMapSymbol({
+          imgPath: 'assets/lore/macro-factions/nova-republic.jpeg',
+          name: 'Nova Republic',
+          color: 'blue',
+          left: '48%',
+          top: '6%',
+        })}
+        ${renderFactionMapSymbol({
+          imgPath: 'assets/lore/macro-factions/zenith-empire.jpeg',
+          name: 'Zenith Empire',
+          color: 'red',
+          left: '42%',
+          top: '76%',
+        })}
+        ${renderFactionMapSymbol({
+          imgPath: 'assets/lore/macro-factions/nova-republic.jpeg',
+          name: 'Nova Republic',
+          color: 'blue',
+          left: '67%',
+          top: '63%',
+        })}
+        ${renderFactionMapSymbol({
+          imgPath: 'assets/lore/macro-factions/atlas-confederation.jpeg',
+          name: 'Atlas Confederation',
+          color: 'yellow',
+          left: '37%',
+          top: '51%',
         })}
       </div>
     `;
