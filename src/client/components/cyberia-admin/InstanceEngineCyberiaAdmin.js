911bef5f59b4b1f6557defb60340f07a0e67fefc
fcoverdugoa <fcoverdugoa@underpost.net>
2024-12-06 15:23:12 -0300

    cyberia instance engine advance

--- src/client/components/cyberia-admin/InstanceEngineCyberiaAdmin.js
+++ src/client/components/cyberia-admin/InstanceEngineCyberiaAdmin.js
@@ -1,13 +1,20 @@
+import { BtnIcon } from '../core/BtnIcon.js';
+import { dynamicCol } from '../core/Css.js';
 import { s } from '../core/VanillaJs.js';
 import { CyberiaInstancesStructs } from '../cyberia/CommonCyberia.js';
 import { createJSONEditor } from 'vanilla-jsoneditor';
 // https://www.npmjs.com/package/vanilla-jsoneditor
 
 const InstanceEngineCyberiaAdmin = {
-  Render: async function () {
+  Render: async function (options = { idModal: '' }) {
+    const engineCyberiaInstanceMenu = {
+      'cyberia-instance-btn-json': {},
+      'cyberia-instance-btn-management': {},
+    };
+
     setTimeout(() => {
       let content = {
-        text: JSON.stringify(CyberiaInstancesStructs.default, null, 4),
+        text: undefined, // JSON.stringify(CyberiaInstancesStructs.default, null, 4)
         json: CyberiaInstancesStructs.default,
       };
       const editor = createJSONEditor({
@@ -22,7 +29,29 @@ const InstanceEngineCyberiaAdmin = {
         },
       });
     });
-    return html` <div class="jsoneditor"></div> `;
+    const dynamicColId = 'cyberia-instance-dynamic-col';
+    return html`
+      ${dynamicCol({ containerSelector: options.idModal, id: dynamicColId })}
+      <div class="fl">
+        <div class="in fll ${dynamicColId}-col-a">
+          <div class="in">
+            ${await BtnIcon.Render({
+              class: `inl section-mp btn-custom cyberia-instance-btn-json`,
+              label: html`json`,
+            })}
+          </div>
+          <div class="in">
+            ${await BtnIcon.Render({
+              class: `inl section-mp btn-custom cyberia-instance-btn-management`,
+              label: html`management`,
+            })}
+          </div>
+        </div>
+        <div class="in fll ${dynamicColId}-col-b">
+          <div class="jsoneditor"></div>
+        </div>
+      </div>
+    `;
   },
 };
 
