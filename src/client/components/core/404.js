0c56d1516926e24f5b0a17cbb288585a8f9a5efb
fcoverdugoa <fcoverdugoa@underpost.net>
2024-12-10 15:19:54 -0300

    add default page 404 and 500

--- src/client/components/core/404.js
+++ src/client/components/core/404.js
@@ -1,8 +1,19 @@
 import { Alert } from './Alert.js';
+import { Modal } from './Modal.js';
+import { s } from './VanillaJs.js';
 
 const Page404 = {
-  Render: async function ({ idModal }) {
-    return html`${await Alert.e404()}`;
+  Render: async function (options = { idModal: '' }) {
+    setTimeout(() => {
+      Modal.Data[options.idModal].onObserverListener['404'] = () => {
+        if (s(`.container-404-${options.idModal}`))
+          s(`.container-404-${options.idModal}`).style.height = `${
+            s(`.${options.idModal}`).offsetHeight - Modal.headerTitleHeight
+          }px`;
+      };
+      Modal.Data[options.idModal].onObserverListener['404']();
+    });
+    return html`<div class="in container-404-${options.idModal}">${await Alert.e404()}</div>`;
   },
 };
 
--- src/client/components/core/500.js
+++ src/client/components/core/500.js
@@ -1,8 +1,19 @@
 import { Alert } from './Alert.js';
+import { Modal } from './Modal.js';
+import { s } from './VanillaJs.js';
 
 const Page500 = {
-  Render: async function ({ idModal }) {
-    return html`${await Alert.e500()}`;
+  Render: async function (options = { idModal: '' }) {
+    setTimeout(() => {
+      Modal.Data[options.idModal].onObserverListener['500'] = () => {
+        if (s(`.container-500-${options.idModal}`))
+          s(`.container-500-${options.idModal}`).style.height = `${
+            s(`.${options.idModal}`).offsetHeight - Modal.headerTitleHeight
+          }px`;
+      };
+      Modal.Data[options.idModal].onObserverListener['500']();
+    });
+    return html`<div class="in container-500-${options.idModal}">${await Alert.e500()}</div>`;
   },
 };
 
