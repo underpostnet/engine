719efb7486c2651450ad698f9d5770042ed4c060
fcoverdugoa <fcoverdugoa@underpost.net>
2024-12-12 09:56:01 -0300

    [ci][docker-image][engine] v2.7.92

--- bin/index.js
+++ bin/index.js
@@ -19,7 +19,7 @@ const globalBinFolder = `${shellExec(`npm root -g`, {
 
 const program = new Command();
 
-const version = '2.7.91';
+const version = '2.7.92';
 
 program.name('underpost').description(`underpost.net ci/cd cli ${version}`).version(version);
 
