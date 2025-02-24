ee865ffedf1b0560b94d2923fc3a45195562b8ae
underpostnet <fcoverdugoa@underpost.net>
2025-02-24 17:39:26 -0500

    feat(cli): ✨ add lampp dockerfile node script case

--- src/cli/image.js
+++ src/cli/image.js
@@ -1,6 +1,7 @@
 import fs from 'fs-extra';
 import Underpost from '../index.js';
 import { shellExec } from '../server/process.js';
+import { MariaDB } from '../db/mariadb/MariaDB.js';
 
 class UnderpostImage {
   static API = {
@@ -13,16 +14,47 @@ class UnderpostImage {
         const podManImg = `localhost/${imgName}`;
         const imagesStoragePath = `./images`;
         const tarFile = `${imagesStoragePath}/${imgName.replace(':', '_')}.tar`;
-        if (!imageArchive) {
+        if (imageArchive !== true) {
           shellExec(`cd ${path} && sudo podman build -f ./Dockerfile -t ${imgName} --pull=never`);
           shellExec(`cd ${path} && podman save -o ${tarFile} ${podManImg}`);
         }
         shellExec(`cd ${path} && sudo kind load image-archive ${tarFile}`);
       },
-      script(deployId = 'default', env = 'development') {
+      async script(deployId = 'default', env = 'development') {
         switch (deployId) {
           case 'dd-lampp':
             {
+              const lamppPublicPath = '/xampp/htdocs/online';
+              if (process.argv.includes('test')) {
+                const { MARIADB_HOST, MARIADB_USER, MARIADB_PASSWORD, DD_LAMPP_TEST_DB_0 } = process.env;
+
+                await MariaDB.query({
+                  host: MARIADB_HOST,
+                  user: MARIADB_USER,
+                  password: MARIADB_PASSWORD,
+                  query: `SHOW TABLES FROM ${DD_LAMPP_TEST_DB_0}`,
+                });
+                process.exit(0);
+              }
+              shellExec(`sudo mkdir -p ${lamppPublicPath}`);
+
+              {
+                shellExec(
+                  `cd ${lamppPublicPath} && git clone https://${process.env.GITHUB_TOKEN}@github.com/${process.env.DD_LAMPP_REPO_0}`,
+                );
+
+                shellExec(`cd ${lamppPublicPath} && sudo ${process.env.DD_LAMPP_SCRIPT_0}`);
+
+                shellExec(
+                  `sudo sed -i -e "s@define( 'DB_HOST', 'localhost' );@define( 'DB_HOST', '${MARIADB_HOST}' );@g" ${lamppPublicPath}/${process.env.DD_LAMPP_REPO_0_FOLDER}/wp-config.php`,
+                );
+              }
+
+              {
+                shellExec(
+                  `cd ${lamppPublicPath} && git clone https://${process.env.GITHUB_TOKEN}@github.com/${process.env.DD_LAMPP_REPO_1}`,
+                );
+              }
             }
             break;
 
