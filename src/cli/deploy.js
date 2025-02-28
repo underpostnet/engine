85fb7971e173e1bd08e81a27c5c316bd69384a1f
underpostnet <fcoverdugoa@underpost.net>
2025-02-28 11:39:05 -0500

    feat(cli): ✨ deploy getPods method

--- src/cli/deploy.js
+++ src/cli/deploy.js
@@ -1,3 +1,5 @@
+import { shellExec } from '../server/process.js';
+
 class UnderpostDeploy {
   static API = {
     callback: (deployList = 'default', env = 'development') => {
@@ -5,6 +7,37 @@ class UnderpostDeploy {
         const deployId = _deployId.trim();
         if (!deployId) continue;
       }
+    },
+    getPods(deployId) {
+      const raw = shellExec(`sudo kubectl get pods --all-namespaces -o wide`, {
+        stdout: true,
+        disableLog: false,
+        silent: true,
+      });
+
+      const heads = raw
+        .split(`\n`)[0]
+        .split(' ')
+        .filter((_r) => _r.trim());
+
+      const pods = raw
+        .split(`\n`)
+        .filter((r) => (deployId ? r.match(deployId) : r.trim() && !r.match('NAME')))
+        .map((r) => r.split(' ').filter((_r) => _r.trim()));
+
+      const result = [];
+
+      for (const row of pods) {
+        const pod = {};
+        let index = -1;
+        for (const head of heads) {
+          index++;
+          pod[head] = row[index];
+        }
+        result.push(pod);
+      }
+
+      return result;
     },
   };
 }
