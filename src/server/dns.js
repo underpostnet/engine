9bb9fe7d7b5273c81c1b6a902f62d1d51bd63f6a
underpostnet <fcoverdugoa@underpost.net>
2025-03-03 17:34:02 -0500

    fix: 🐛 A bug fix

--- src/server/dns.js
+++ src/server/dns.js
@@ -28,7 +28,7 @@ class Dns {
     // LAN server or device's local servers port ->  3000-3100 (2999-3101)
     // DNS Records: [ANAME](Address Dynamic) -> [A](ipv4) host | [AAAA](ipv6) host -> [public-ip]
     // Forward the router's TCP/UDP ports to the LAN device's IP address
-    for (const _deployId of deployList.trim()) {
+    for (const _deployId of deployList.split(',')) {
       const deployId = _deployId.trim();
     const privateCronConfPath = `./engine-private/conf/${deployId}/conf.cron.json`;
     const confCronPath = fs.existsSync(privateCronConfPath) ? privateCronConfPath : './conf/conf.cron.json';
