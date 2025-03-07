a2bf5c5c574666bc1420396959e845abaac9234a
underpostnet <fcoverdugoa@underpost.net>
2025-03-07 11:02:54 -0500

    chore: ♻️ Other changes that don't modify src or test files

--- src/cli/fs.js
+++ src/cli/fs.js
@@ -1,6 +1,12 @@
 class UnderpostFileStorage {
   static API = {
-    async callback(deployList, path) {},
+    async callback(deployList, path) {
+      for (const _deployId of deployList.split(',')) {
+        const deployId = _deployId.trim();
+        if (!deployId) continue;
+        console.log({ deployId });
+      }
+    },
   };
 }
 
