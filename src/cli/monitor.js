4b234f7ab419745946731dfa9542118f29fa794d
underpostnet <fcoverdugoa@underpost.net>
2025-06-07 13:12:07 -0400

    Add cluster config map monitor update

--- src/cli/monitor.js
+++ src/cli/monitor.js
@@ -120,6 +120,11 @@ class UnderpostMonitor {
                           fs.readFileSync(`./engine-private/conf/${deployId}/conf.server.json`, 'utf8'),
                         );
 
+                        shellExec(`kubectl delete configmap underpost-config`);
+                        shellExec(
+                          `kubectl create configmap underpost-config --from-file=/home/dd/engine/engine-private/conf/dd-cron/.env.${env}`,
+                        );
+
                         for (const host of Object.keys(confServer)) {
                           shellExec(`sudo kubectl delete HTTPProxy ${host}`);
                         }
