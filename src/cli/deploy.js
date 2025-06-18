d3cf8313054f584595346d4bc76830c81298dbce
underpostnet <fcoverdugoa@underpost.net>
2025-06-18 03:58:57 -0400

    Add info util cli

--- src/cli/deploy.js
+++ src/cli/deploy.js
@@ -261,6 +261,10 @@ kubectl exec -it <pod-name> -- bash
 kubectl exec -it <pod-name> -- sh
 docker exec -it kind-control-plane bash
 curl -4 -v google.com
+kubectl taint nodes <node-name> node-role.kubernetes.io/control-plane:NoSchedule-
+kubectl run test-pod --image=busybox:latest --restart=Never -- /bin/sh -c "while true; do sleep 30; done;"
+kubectl run test-pod --image=alpine/curl:latest --restart=Never -- sh -c "sleep infinity"
+kubectl get node <node-name> -o jsonpath='{.spec.podCIDR}'
 `);
       if (deployList === 'dd' && fs.existsSync(`./engine-private/deploy/dd.router`))
         deployList = fs.readFileSync(`./engine-private/deploy/dd.router`, 'utf8');
