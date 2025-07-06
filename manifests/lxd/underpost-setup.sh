fa95c2cd624920bec9ab606581ea246f1e4c4016
underpostnet <fcoverdugoa@underpost.net>
2025-07-06 16:19:52 -0400

    Add ufw allow underpost setup sh

--- manifests/lxd/underpost-setup.sh
+++ manifests/lxd/underpost-setup.sh
@@ -27,6 +27,9 @@ resize2fs /dev/sda2
 echo "Disk and filesystem resized successfully."
 sudo dnf install -y tar
 sudo dnf install -y bzip2
+sudo dnf install -y git
+sudo dnf install -y ufw
+sudo systemctl enable --now ufw
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
@@ -76,6 +79,51 @@ echo "USE_WORKER = $USE_WORKER"
 
 underpost cluster --kubeadm
 underpost cluster --reset
+
+PORTS=(
+    22    # SSH
+    80    # HTTP
+    443   # HTTPS
+    53    # DNS (TCP/UDP)
+    66    # TFTP
+    67    # DHCP
+    69    # TFTP
+    111   # rpcbind
+    179   # Calico BGP
+    2049  # NFS
+    20048 # NFS mountd
+    4011  # PXE boot
+    5240  # snapd API
+    5248  # Juju controller
+    6443  # Kubernetes API
+    9153  # CoreDNS metrics
+    10250 # Kubelet API
+    10251 # kube-scheduler
+    10252 # kube-controller-manager
+    10255 # Kubelet read-only (deprecated)
+    10257 # controller-manager (v1.23+)
+    10259 # scheduler (v1.23+)
+)
+
+PORT_RANGES=(
+    2379:2380 # etcd
+    # 30000:32767 # NodePort range
+    # 3000:3100 # App node ports
+    32765:32766 # Ephemeral ports
+    6783:6784   # Weave Net
+)
+
+# Open individual ports
+for PORT in "${PORTS[@]}"; do
+    ufw allow ${PORT}/tcp
+    ufw allow ${PORT}/udp
+done
+
+# Open port ranges
+for RANGE in "${PORT_RANGES[@]}"; do
+    ufw allow ${RANGE}/tcp
+    ufw allow ${RANGE}/udp
+done
 
 # Behavior based on flags
 if $USE_KUBEADM; then
