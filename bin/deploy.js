1bdbcd94da5254ad8f3c2adbef5e2cfd66de8288
underpostnet <fcoverdugoa@underpost.net>
2025-07-16 06:53:26 -0400

    Update chrony servers pool

--- bin/deploy.js
+++ bin/deploy.js
@@ -2571,11 +2571,45 @@ nvidia/gpu-operator \
 
       fs.writeFileSync(
         `/etc/chrony.conf`,
-        `server 0.pool.ntp.org iburst
-allow 192.168.1.0/24
+        `
+# Use public servers from the pool.ntp.org project.
+# Please consider joining the pool (http://www.pool.ntp.org/join.html).
+pool 2.pool.ntp.org iburst
+
+# Record the rate at which the system clock gains/losses time.
 driftfile /var/lib/chrony/drift
+
+# Allow the system clock to be stepped in the first three updates
+# if its offset is larger than 1 second.
 makestep 1.0 3
+
+# Enable kernel synchronization of the real-time clock (RTC).
 rtcsync
+
+# Enable hardware timestamping on all interfaces that support it.
+#hwtimestamp *
+
+# Increase the minimum number of selectable sources required to adjust
+# the system clock.
+#minsources 2
+
+# Allow NTP client access from local network.
+#allow 192.168.0.0/16
+
+# Serve time even if not synchronized to a time source.
+#local stratum 10
+
+# Specify file containing keys for NTP authentication.
+keyfile /etc/chrony.keys
+
+# Get TAI-UTC offset and leap seconds from the system tz database.
+leapsectz right/UTC
+
+# Specify directory for log files.
+logdir /var/log/chrony
+
+# Select which information is logged.
+#log measurements statistics tracking
 `,
         'utf8',
       );
@@ -2583,10 +2617,15 @@ rtcsync
       shellExec(`sudo systemctl enable --now chronyd`);
       shellExec(`sudo systemctl restart chronyd`);
       shellExec(`sudo systemctl status chronyd`);
+
+      shellExec(`chronyd -q 'server 0.europe.pool.ntp.org iburst'`);
+
       shellExec(`chronyc sources`);
       shellExec(`chronyc tracking`);
       // sudo firewall-cmd --add-service=ntp --permanent
       // sudo firewall-cmd --reload
+
+      shellExec(`chronyc sourcestats -v`);
 
       break;
     }
