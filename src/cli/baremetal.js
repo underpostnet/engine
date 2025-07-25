131f033706561a7d4281100d7b22bb8bf2094541
underpostnet <fcoverdugoa@underpost.net>
2025-07-25 19:18:00 -0400

    Update crossArchBinFactory and crossArchRunner baremetal cli methods

--- src/cli/baremetal.js
+++ src/cli/baremetal.js
@@ -147,6 +147,28 @@ class UnderpostBaremetal {
         if (!isMounted) {
           UnderpostBaremetal.API.nfsMountCallback({ hostname, workflowId, mount: true });
         }
+
+        {
+          const { systemProvisioning, kernelLibVersion, chronyc } = UnderpostBaremetal.API.workflowsConfig[workflowId];
+          const { timezone, chronyConfPath } = chronyc;
+
+          UnderpostBaremetal.API.crossArchRunner({
+            nfsHostPath,
+            debootstrapArch,
+            callbackMetaData,
+            steps: [
+              ...UnderpostBaremetal.API.systemProvisioningFactory[systemProvisioning].base({
+                kernelLibVersion,
+              }),
+              ...UnderpostBaremetal.API.systemProvisioningFactory[systemProvisioning].user(),
+              ...UnderpostBaremetal.API.systemProvisioningFactory[systemProvisioning].timezone({
+                timezone,
+                chronyConfPath,
+              }),
+              ...UnderpostBaremetal.API.systemProvisioningFactory[systemProvisioning].keyboard(),
+            ],
+          });
+        }
       }
 
       if (options.commission === true) {
@@ -168,7 +190,7 @@ class UnderpostBaremetal {
       }
     },
 
-    crossArchRunner({ nfsHostPath, debootstrapArch, callbackMetaData, steps: [] }) {
+    crossArchRunner({ nfsHostPath, debootstrapArch, callbackMetaData, steps }) {
       steps = UnderpostBaremetal.API.stepsRender(steps, false);
 
       let qemuCrossArchBash = '';
@@ -245,8 +267,116 @@ EOF`);
       throw new Error(`Unsupported host architecture: ${machine}`);
     },
 
+    systemProvisioningFactory: {
+      ubuntu: {
+        base: ({ kernelLibVersion }) => [
+          `cat <<EOF | tee /etc/apt/sources.list
+deb http://ports.ubuntu.com/ubuntu-ports noble main restricted universe multiverse
+deb http://ports.ubuntu.com/ubuntu-ports noble-updates main restricted universe multiverse
+deb http://ports.ubuntu.com/ubuntu-ports noble-security main restricted universe multiverse
+EOF`,
+
+          `apt update -qq`,
+          `apt -y full-upgrade`,
+          `apt install -y build-essential xinput x11-xkb-utils usbutils`,
+          'apt install -y linux-image-generic',
+          `apt install -y linux-modules-${kernelLibVersion} linux-modules-extra-${kernelLibVersion}`,
+
+          `depmod -a ${kernelLibVersion}`,
+          // `apt install -y cloud-init=25.1.2-0ubuntu0~24.04.1`,
+          `apt install -y cloud-init systemd-sysv openssh-server sudo locales udev util-linux systemd-sysv iproute2 netplan.io ca-certificates curl wget chrony`,
+          `ln -sf /lib/systemd/systemd /sbin/init`,
+
+          `apt-get update`,
+          `DEBIAN_FRONTEND=noninteractive apt-get install -y apt-utils`,
+          `DEBIAN_FRONTEND=noninteractive apt-get install -y tzdata kmod keyboard-configuration console-setup iputils-ping`,
+        ],
+        user: () => [
+          `useradd -m -s /bin/bash -G sudo root`,
+          `echo 'root:root' | chpasswd`,
+          `mkdir -p /home/root/.ssh`,
+          `echo '${fs.readFileSync(
+            `/home/dd/engine/engine-private/deploy/id_rsa.pub`,
+            'utf8',
+          )}' > /home/root/.ssh/authorized_keys`,
+          `chown -R root /home/root/.ssh`,
+          `chmod 700 /home/root/.ssh`,
+          `chmod 600 /home/root/.ssh/authorized_keys`,
+        ],
+        timezone: ({ timezone, chronyConfPath }, alias = 'chrony') => [
+          `export DEBIAN_FRONTEND=noninteractive`,
+          `ln -fs /usr/share/zoneinfo/${timezone} /etc/localtime`,
+          `sudo dpkg-reconfigure --frontend noninteractive tzdata`,
+
+          `echo '
+# Use public servers from the pool.ntp.org project.
+# Please consider joining the pool (http://www.pool.ntp.org/join.html).
+# pool 2.pool.ntp.org iburst
+server ${process.env.MAAS_NTP_SERVER} iburst
+
+# Record the rate at which the system clock gains/losses time.
+driftfile /var/lib/chrony/drift
+
+# Allow the system clock to be stepped in the first three updates
+# if its offset is larger than 1 second.
+makestep 1.0 3
+
+# Enable kernel synchronization of the real-time clock (RTC).
+rtcsync
+
+# Enable hardware timestamping on all interfaces that support it.
+#hwtimestamp *
+
+# Increase the minimum number of selectable sources required to adjust
+# the system clock.
+#minsources 2
+
+# Allow NTP client access from local network.
+#allow 192.168.0.0/16
+
+# Serve time even if not synchronized to a time source.
+#local stratum 10
+
+# Specify file containing keys for NTP authentication.
+keyfile /etc/chrony.keys
+
+# Get TAI-UTC offset and leap seconds from the system tz database.
+leapsectz right/UTC
+
+# Specify directory for log files.
+logdir /var/log/chrony
+
+# Select which information is logged.
+#log measurements statistics tracking
+' > ${chronyConfPath} `,
+          `systemctl stop ${alias}`,
+
+          // `${alias}d -q 'server ntp.ubuntu.com iburst'`,
+          // `${alias}d -q 'server 0.europe.pool.ntp.org iburst'`,
+
+          `sudo systemctl enable --now ${alias}`,
+          `sudo systemctl restart ${alias}`,
+          `sudo systemctl status ${alias}`,
+
+          `chronyc sources`,
+          `chronyc tracking`,
+
+          `chronyc sourcestats -v`,
+          `timedatectl status`,
+        ],
+        keyboard: () => [
+          `sudo locale-gen en_US.UTF-8`,
+          `sudo update-locale LANG=en_US.UTF-8`,
+          `sudo sed -i 's/XKBLAYOUT="us"/XKBLAYOUT="es"/' /etc/default/keyboard`,
+          `sudo dpkg-reconfigure --frontend noninteractive keyboard-configuration`,
+          `sudo systemctl restart keyboard-setup.service`,
+        ],
+      },
+    },
+
     workflowsConfig: {
       rpi4mb: {
+        systemProvisioning: 'ubuntu',
         kernelLibVersion: `6.8.0-41-generic`,
         chronyc: {
           timezone: 'America/New_York',
