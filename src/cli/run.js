1d45b7febd05c66d14d4d2afd75692244db8a8c6
underpostnet <fcoverdugoa@underpost.net>
2025-08-01 13:34:13 -0400

    Add gpu tf test script

--- src/cli/run.js
+++ src/cli/run.js
@@ -5,7 +5,21 @@ import UnderpostRootEnv from './env.js';
 
 class UnderpostRun {
   static API = {
-    callback(path, options = {}) {},
+    callback(path, options = {}) {
+      const fileName = path.split('/').pop();
+
+      switch (fileName) {
+        case 'gpu': {
+          shellExec(
+            `node bin cluster --dev --reset && node bin cluster --dev --dedicated-gpu --kubeadm && kubectl get pods --all-namespaces -o wide -w`,
+          );
+          break;
+        }
+        case 'tf':
+          shellExec(`kubectl apply -f manifests/deployment/tensorflow/tf-gpu-test.yaml`);
+          break;
+      }
+    },
   };
 }
 
