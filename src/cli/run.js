4d818d9515aa9b7d16a7a060482dcd7b896a603e
underpostnet <fcoverdugoa@underpost.net>
2025-08-01 15:26:58 -0400

    Add run spark-template script

--- src/cli/run.js
+++ src/cli/run.js
@@ -1,14 +1,34 @@
 import dotenv from 'dotenv';
-import { shellExec } from '../server/process.js';
+import { pbcopy, shellCd, shellExec } from '../server/process.js';
 import fs from 'fs-extra';
 import UnderpostRootEnv from './env.js';
+import read from 'read';
 
 class UnderpostRun {
   static API = {
-    callback(path, options = {}) {
+    async callback(path, options = {}) {
       const fileName = path.split('/').pop();
 
       switch (fileName) {
+        case 'spark-template': {
+          const path = '/home/dd/spark-template';
+          shellExec(`sudo rm -rf ${path}`);
+          shellCd('/home/dd');
+
+          pbcopy(`cd /home/dd && sbt new underpostnet/spark-template.g8`);
+          await read({ prompt: 'Command copy to clipboard, press enter to continue.\n' });
+          shellCd(path);
+
+          shellExec(`git init && git add . && git commit -m "Base implementation"`);
+          shellExec(`chmod +x ./replace_params.sh`);
+          shellExec(`chmod +x ./build.sh`);
+
+          shellExec(`./replace_params.sh`);
+          shellExec(`./build.sh`);
+
+          shellCd('/home/dd/engine');
+          break;
+        }
         case 'gpu': {
           shellExec(
             `node bin cluster --dev --reset && node bin cluster --dev --dedicated-gpu --kubeadm && kubectl get pods --all-namespaces -o wide -w`,
