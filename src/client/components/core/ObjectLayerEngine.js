3923cad7417b7abf39028525db93f7577bf45308
underpostnet <fcoverdugoa@underpost.net>
2025-08-31 09:37:26 -0400

    WebComponent test

---
+++ src/client/components/core/ObjectLayerEngine.js
@@ -0,0 +1,1 @@
+console.warn('Test object layer engine component');
--- src/client/components/cyberia-portal/MenuCyberiaPortal.js
+++ src/client/components/cyberia-portal/MenuCyberiaPortal.js
@@ -148,15 +148,19 @@ const MenuCyberiaPortal = {
         </div>
       `,
       htmlMainBody: async () => {
-        return await ServerCyberiaPortal.Render({
-          idModal: 'modal-server-body',
-          events: {
-            'change-server-body': async ({ name }) => {
-              const { protocol, hostname } = window.location;
-              return (location.href = `${protocol}//${hostname}/${name}`);
-            },
-          },
-        });
+        // return await ServerCyberiaPortal.Render({
+        //   idModal: 'modal-server-body',
+        //   events: {
+        //     'change-server-body': async ({ name }) => {
+        //       const { protocol, hostname } = window.location;
+        //       return (location.href = `${protocol}//${hostname}/${name}`);
+        //     },
+        //   },
+        // });
+        await import(`${getProxyPath()}components/core/ObjectLayerEngine.js`);
+        await import(`${getProxyPath()}components/core/WebComponent.js`);
+
+        return html`<my-element>test</my-element>`;
       },
       barConfig: newInstance(barConfig),
       title: NameApp,
