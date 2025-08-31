e3d39ab65a2826f3209b60a7bd87347a6cdfa37e
underpostnet <fcoverdugoa@underpost.net>
2025-08-31 12:55:22 -0400

    Update src/client/components/cyberia-portal/CssCyberiaPortal.js remove legacy backgourn logic

--- src/client/components/cyberia-portal/CssCyberiaPortal.js
+++ src/client/components/cyberia-portal/CssCyberiaPortal.js
@@ -2,14 +2,10 @@ import { newInstance } from '../core/CommonJs.js';
 import { s } from '../core/VanillaJs.js';
 import { CssCommonCyberia } from '../cyberia/CssCyberia.js';
 
-let backGroundImage;
-
 const CssCyberiaPortalDark = {
   theme: 'cyberia-portal-dark',
   dark: true,
   render: async () => {
-    s(`.ssr-background-image`).style.backgroundImage = backGroundImage;
-
     return (await CssCommonCyberia()) + html` <style></style> `;
   },
 };
@@ -18,9 +14,6 @@ const CssCyberiaPortalLight = {
   theme: 'cyberia-portal-light',
   dark: false,
   render: async () => {
-    if (!backGroundImage) backGroundImage = newInstance(s(`.ssr-background-image`).style.backgroundImage);
-    s(`.ssr-background-image`).style.backgroundImage = 'none';
-
     return (await CssCommonCyberia()) + html` <style></style> `;
   },
 };
