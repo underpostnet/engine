73ff9d9d911f857c4376ba00497d157086c7bdbe
underpostnet <fcoverdugoa@underpost.net>
2025-08-31 20:59:00 -0400

    Refactor cyberia portal style

--- src/client/components/cyberia-portal/MenuCyberiaPortal.js
+++ src/client/components/cyberia-portal/MenuCyberiaPortal.js
@@ -174,13 +174,6 @@ const MenuCyberiaPortal = {
       title: NameApp,
       // titleClass: 'hide',
       titleRender: () => {
-        ThemeEvents['titleRender'] = () => {
-          const srcLogo = darkTheme
-            ? `${getProxyPath()}assets/splash/favicon-white-alpha.png`
-            : `${getProxyPath()}assets/splash/favicon-black-alpha.png`;
-          htmls('.action-btn-app-icon-render', html`<img class="inl top-bar-app-icon" src="${srcLogo}" />`);
-        };
-        setTimeout(ThemeEvents['titleRender']);
         return '';
       },
       // mode: 'slide-menu-right',
@@ -238,6 +231,34 @@ const MenuCyberiaPortal = {
         // evt.pullMode; // when item is in another sortable: `"clone"` if cloning, `true` if moving
       },
     });
+
+    ThemeEvents['portal-main-theme-event'] = () => {
+      const srcLogo = darkTheme
+        ? `${getProxyPath()}assets/splash/favicon-white-alpha.png`
+        : `${getProxyPath()}assets/splash/favicon-black-alpha.png`;
+      htmls('.action-btn-app-icon-render', html`<img class="inl top-bar-app-icon" src="${srcLogo}" />`);
+
+      if (darkTheme) {
+        htmls(
+          `.style-ssr-background-image`,
+          css`
+            .ssr-background-image {
+              background: #191919;
+            }
+          `,
+        );
+      } else {
+        htmls(
+          `.style-ssr-background-image`,
+          css`
+            .ssr-background-image {
+              background: #e8e8e8;
+            }
+          `,
+        );
+      }
+    };
+    setTimeout(ThemeEvents['portal-main-theme-event']);
 
     EventsUI.onClick(`.main-btn-sign-up`, async () => {
       const { barConfig } = await Themes[Css.currentTheme]();
