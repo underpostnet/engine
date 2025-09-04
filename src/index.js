765057f4112cd2f662836d0b69475212c778b516
underpostnet <fcoverdugoa@underpost.net>
2025-09-03 20:57:45 -0400

    ci(package-pwa-microservices-template): ⚙️ CI pipeline changes (GitHub Actions, runners, caching)

--- src/index.js
+++ src/index.js
@@ -35,7 +35,7 @@ class Underpost {
    * @type {String}
    * @memberof Underpost
    */
-  static version = 'v2.8.851';
+  static version = 'v2.8.852';
   /**
    * Repository cli API
    * @static
