4a520c44c831acc17efedaf6e525431b746d618c
underpostnet <fcoverdugoa@underpost.net>
2025-09-06 10:52:19 -0400

    Update src/client/components/core/LogIn.js

--- src/client/components/core/LogIn.js
+++ src/client/components/core/LogIn.js
@@ -108,6 +108,8 @@ const LogIn = {
           setTimeout(() => {
             if (s(`.modal-log-in`)) s(`.btn-close-modal-log-in`).click();
             if (s(`.modal-sign-up`)) s(`.btn-close-modal-sign-up`).click();
+            if (!s(`.main-body-btn-ui-bar-custom-open`).classList.contains('hide'))
+              s(`.main-body-btn-ui-bar-custom-open`).click();
           });
         }
         if (result.status === 'error' && result.message.match('attempts')) {
