a4f283d17d6479b8422f03d7b595ff05dbe88b75
underpostnet <fcoverdugoa@underpost.net>
2025-09-11 16:55:49 -0400

    fIX deploy save conf logic

--- bin/deploy.js
+++ bin/deploy.js
@@ -46,9 +46,21 @@ try {
         const folder = `./engine-private/conf/${deployId}`;
         if (fs.existsSync(folder)) fs.removeSync(folder);
         await Config.build({ folder });
-        fs.writeFileSync(`${folder}/.env.production`, fs.readFileSync('./.env.production', 'utf8'), 'utf8');
-        fs.writeFileSync(`${folder}/.env.development`, fs.readFileSync('./.env.development', 'utf8'), 'utf8');
-        fs.writeFileSync(`${folder}/.env.test`, fs.readFileSync('./.env.test', 'utf8'), 'utf8');
+        fs.writeFileSync(
+          `${folder}/.env.production`,
+          fs.readFileSync('./.env.production', 'utf8').replace('dd-default', deployId),
+          'utf8',
+        );
+        fs.writeFileSync(
+          `${folder}/.env.development`,
+          fs.readFileSync('./.env.development', 'utf8').replace('dd-default', deployId),
+          'utf8',
+        );
+        fs.writeFileSync(
+          `${folder}/.env.test`,
+          fs.readFileSync('./.env.test', 'utf8').replace('dd-default', deployId),
+          'utf8',
+        );
         fs.writeFileSync(`${folder}/package.json`, fs.readFileSync('./package.json', 'utf8'), 'utf8');
       }
       break;
