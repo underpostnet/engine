98b87f669cfd0f1bcdb606751c7050be434db3c1
underpostnet <fcoverdugoa@underpost.net>
2025-09-17 22:35:37 -0400

    Update ol option import imte type cyberia cli

--- bin/cyberia0.js
+++ bin/cyberia0.js
@@ -193,49 +193,51 @@ const generateRandomStats = () => {
 
 program
   .command('ol')
-  .option('--import [object-layer-type]', 'Import object layer from type storage png image')
+  .option('--import [object-layer-type]', 'Commas separated object layer types e.g. skin,floors')
   .option('--show-frame <show-frame-input>', 'View object layer frame e.g. anon_08_0')
   .action(async (options = { import: false, showFrame: '' }) => {
     const objectLayers = {};
 
     if (options.import || options.showFrame) {
-      await pngDirectoryIteratorByObjectLayerType(
-        options.import,
-        async ({ path, objectLayerType, objectLayerId, direction, frame }) => {
-          if (options.showFrame && !`${objectLayerId}_${direction}_${frame}`.match(options.showFrame)) return;
-          console.log(path, { objectLayerType, objectLayerId, direction, frame });
-          if (!objectLayers[objectLayerId]) {
-            const metadataPath = `./src/client/public/cyberia/assets/${objectLayerType}/${objectLayerId}/metadata.json`;
-            const metadata = fs.existsSync(metadataPath) ? JSON.parse(fs.readFileSync(metadataPath, 'utf8')) : null;
-            objectLayers[objectLayerId] = metadata
-              ? metadata
-              : {
-                  data: {
-                    render: {
-                      frame_duration: 250,
-                      is_stateless: false,
+      for (const argItemType of options.import.split(',')) {
+        await pngDirectoryIteratorByObjectLayerType(
+          argItemType,
+          async ({ path, objectLayerType, objectLayerId, direction, frame }) => {
+            if (options.showFrame && !`${objectLayerId}_${direction}_${frame}`.match(options.showFrame)) return;
+            console.log(path, { objectLayerType, objectLayerId, direction, frame });
+            if (!objectLayers[objectLayerId]) {
+              const metadataPath = `./src/client/public/cyberia/assets/${objectLayerType}/${objectLayerId}/metadata.json`;
+              const metadata = fs.existsSync(metadataPath) ? JSON.parse(fs.readFileSync(metadataPath, 'utf8')) : null;
+              objectLayers[objectLayerId] = metadata
+                ? metadata
+                : {
+                    data: {
+                      render: {
+                        frame_duration: 250,
+                        is_stateless: false,
+                      },
+                      item: {
+                        id: objectLayerId,
+                        type: objectLayerType,
+                        description: '',
+                        activable: true,
+                      },
+                      stats: generateRandomStats(),
                     },
-                    item: {
-                      id: objectLayerId,
-                      type: objectLayerType,
-                      description: '',
-                      activable: true,
-                    },
-                    stats: generateRandomStats(),
-                  },
-                };
-            objectLayers[objectLayerId].data.render.colors = [];
-            objectLayers[objectLayerId].data.render.frames = {};
-          }
-          const frameFactoryResult = await frameFactory(path, objectLayers[objectLayerId].data.render.colors);
-          objectLayers[objectLayerId].data.render.colors = frameFactoryResult.colors;
-          for (const objectLayerFrameDirection of getKeyFramesDirectionsFromNumberFolderDirection(direction)) {
-            if (!objectLayers[objectLayerId].data.render.frames[objectLayerFrameDirection])
-              objectLayers[objectLayerId].data.render.frames[objectLayerFrameDirection] = [];
-            objectLayers[objectLayerId].data.render.frames[objectLayerFrameDirection].push(frameFactoryResult.frame);
-          }
-        },
-      );
+                  };
+              objectLayers[objectLayerId].data.render.colors = [];
+              objectLayers[objectLayerId].data.render.frames = {};
+            }
+            const frameFactoryResult = await frameFactory(path, objectLayers[objectLayerId].data.render.colors);
+            objectLayers[objectLayerId].data.render.colors = frameFactoryResult.colors;
+            for (const objectLayerFrameDirection of getKeyFramesDirectionsFromNumberFolderDirection(direction)) {
+              if (!objectLayers[objectLayerId].data.render.frames[objectLayerFrameDirection])
+                objectLayers[objectLayerId].data.render.frames[objectLayerFrameDirection] = [];
+              objectLayers[objectLayerId].data.render.frames[objectLayerFrameDirection].push(frameFactoryResult.frame);
+            }
+          },
+        );
+      }
       for (const objectLayerId of Object.keys(objectLayers)) {
         objectLayers[objectLayerId].sha256 = crypto
           .createHash('sha256')
