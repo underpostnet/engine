8f489a1165d085149942b03973f84fbfb6f6bfd4
underpostnet <fcoverdugoa@underpost.net>
2025-09-22 18:20:40 -0400

    Remove pathfinding dependencie

--- src/client/ssr/head/CyberiaScripts.js
+++ src/client/ssr/head/CyberiaScripts.js
@@ -1,6 +1,5 @@
 SrrComponent = ({ ssrPath }) => html`
   <script type="text/javascript" src="${ssrPath}dist/validator/validator.min.js"></script>
-  <script type="text/javascript" src="${ssrPath}dist/pathfinding/pathfinding-browser.min.js"></script>
   <script type="text/javascript" src="${ssrPath}dist/easymde/easymde.min.js"></script>
   <link rel="stylesheet" href="${ssrPath}dist/easymde/easymde.min.css" />
 `;
