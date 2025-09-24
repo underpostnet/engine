b276b3beb990a3f11385a548426d351ca96fb6b6
underpostnet <fcoverdugoa@underpost.net>
2025-09-23 20:00:40 -0400

    Remove bin ssl

--- src/server/ssl.js
+++ src/server/ssl.js
@@ -50,8 +50,7 @@ const buildSSL = async (host) => {
 
           fs.writeFileSync(`./engine-private/ssl/${host}/_ca_bundle.crt`, ca, 'utf8');
           fs.writeFileSync(`./engine-private/ssl/${host}/_ca_full_bundle.crt`, caFull, 'utf8');
-          // TODO: no repeat folderHost match
-          // fs.removeSync(`${sslPath}/${folderHost}`);
+
           return true;
         }
       }
