3180af6e1e682ada0026451d05819d96be55a66c
underpostnet <fcoverdugoa@underpost.net>
2025-11-13 20:25:49 -0300

    Update start.js

--- src/server/start.js
+++ src/server/start.js
@@ -132,7 +132,7 @@ class UnderpostStartUp {
       const buildBasePath = `/home/dd`;
       const repoName = `engine-${deployId.split('-')[1]}`;
       shellExec(`cd ${buildBasePath} && underpost clone ${process.env.GITHUB_USERNAME}/${repoName}`);
-      shellExec(`cd ${buildBasePath} && sudo cp -a ./${repoName} ./engine`);
+      shellExec(`cd ${buildBasePath} && sudo cp -a ./${repoName}/* ./engine`);
       shellExec(`cd ${buildBasePath} && sudo rm -rf ./${repoName}`);
       shellExec(`cd ${buildBasePath}/engine && underpost clone ${process.env.GITHUB_USERNAME}/${repoName}-private`);
       shellExec(`cd ${buildBasePath}/engine && sudo mv ./${repoName}-private ./engine-private`);
