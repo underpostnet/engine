aa98f071dee4646a63c485fdc5aac2454bf93c10
underpostnet <fcoverdugoa@underpost.net>
2025-11-14 11:25:21 -0300

    ci(package-pwa-microservices-template): ⚙️ New release v:2.89.35

--- cli.md
+++ cli.md
@@ -1,4 +1,4 @@
-## underpost ci/cd cli v2.89.3
+## underpost ci/cd cli v2.89.355
 
 ### Usage: `underpost [options] [command]`
   ```
--- src/index.js
+++ src/index.js
@@ -35,7 +35,7 @@ class Underpost {
    * @type {String}
    * @memberof Underpost
    */
-  static version = 'v2.89.3';
+  static version = 'v2.89.35';
   /**
    * Repository cli API
    * @static
