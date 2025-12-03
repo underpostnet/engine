40e8a51f8760a58b4fbe9a2a772a18070dccd8b0
underpostnet <fcoverdugoa@underpost.net>
2025-12-03 00:06:42 -0300

    Fix disable password ssh cli

--- src/cli/ssh.js
+++ src/cli/ssh.js
@@ -42,7 +42,7 @@ class UnderpostSSH {
      * @param {boolean} [options.hostsList=false] - List known SSH hosts
      * @param {boolean} [options.importKeys=false] - Import keys from private backup to user's SSH directory
      * @param {boolean} [options.exportKeys=false] - Export keys from user's SSH directory to private backup
-     * @param {boolean} [options.noPassword=false] - If true, do not set a password for the user
+     * @param {boolean} [options.disablePassword=false] - If true, do not set a password for the user
      * @returns {Promise<void>}
      * @description
      * Handles various SSH operations:
@@ -95,13 +95,13 @@ class UnderpostSSH {
         hostsList: false,
         importKeys: false,
         exportKeys: false,
-        noPassword: false,
+        disablePassword: false,
       },
     ) => {
       let confNode, confNodePath;
       if (!options.user) options.user = 'root';
       if (!options.host) options.host = await Dns.getPublicIp();
-      if (!options.password) options.password = options.noPassword ? '' : generateRandomPasswordSelection(16);
+      if (!options.password) options.password = options.disablePassword ? '' : generateRandomPasswordSelection(16);
       if (!options.groups) options.groups = 'wheel';
       if (!options.port) options.port = 22;
 
