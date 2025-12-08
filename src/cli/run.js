49aed76603a538c983c83a803ebc18e9d16e0b97
underpostnet <fcoverdugoa@underpost.net>
2025-12-08 10:02:38 -0300

    Update run.js

--- src/cli/run.js
+++ src/cli/run.js
@@ -636,7 +636,7 @@ chmod 600 "$SSH_KEY"
 ssh -i "$SSH_KEY" -o BatchMode=yes "$REMOTE_USER@$REMOTE_HOST" -p $REMOTE_PORT sh <<EOF
 cd /home/dd/engine
 sudo -n -- /bin/bash -lc "${[
-        `${baseCommand} run${baseClusterCommand} stop${path ? path : ` ${path}`}`,
+        `${baseCommand} run${baseClusterCommand} stop${path ? ` ${path}` : ''}`,
         ` --deploy-id ${options.deployId}${options.instanceId ? ` --instance-id ${options.instanceId}` : ''}`,
         ` --namespace ${options.namespace}`,
       ].join('')}"
