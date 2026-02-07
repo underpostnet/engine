7db58689c1d6bec538a7a49d8b48f916f77aba7e
underpostnet <fcoverdugoa@underpost.net>
2026-02-07 10:57:18 -0300

    [cli-secrets] Remove legacy docker secrets api

--- src/cli/secrets.js
+++ src/cli/secrets.js
@@ -19,40 +19,6 @@ dotenv.config();
 class UnderpostSecret {
   static API = {
     /**
-     * @method docker
-     * @description Manages the secrets of the application.
-     * @memberof UnderpostSecret
-     */
-    docker: {
-      /**
-       * @method init
-       * @description Initializes the docker secrets.
-       * @memberof UnderpostSecret
-       */
-      init() {
-        shellExec(`docker swarm init`);
-      },
-      /**
-       * @method createFromEnvFile
-       * @description Creates a secret from an env file.
-       * @param {string} envPath - The path to the env file.
-       * @memberof UnderpostSecret
-       */
-      createFromEnvFile(envPath) {
-        const envObj = dotenv.parse(fs.readFileSync(envPath, 'utf8'));
-        for (const key of Object.keys(envObj)) {
-          Underpost.secret.docker.set(key, envObj[key]);
-        }
-      },
-      set(key, value) {
-        shellExec(`docker secret rm ${key}`);
-        shellExec(`echo "${value}" | docker secret create ${key} -`);
-      },
-      list() {
-        shellExec(`docker secret ls`);
-      },
-    },
-    /**
      * @method underpost
      * @description Manages the secrets of the application.
      * @memberof UnderpostSecret
