ba32abeaff4d198c79bcd92ab0fc0120bb41d9d5
underpostnet <fcoverdugoa@underpost.net>
2026-02-22 13:13:13 -0300

    [bin-zed] Move zed settings file tod zed js bin module

--- bin/zed.js
+++ bin/zed.js
@@ -6,9 +6,70 @@ import { getUnderpostRootPath } from '../src/server/conf.js';
 const logger = loggerFactory(import.meta);
 const underpostRoot = getUnderpostRootPath();
 
+const settings = {
+  ui_font_size: 16,
+  buffer_font_size: 15,
+  theme: {
+    mode: 'system',
+    light: 'One Dark',
+    dark: 'One Dark',
+  },
+
+  features: {
+    edit_prediction_provider: 'copilot',
+    copilot: true,
+  },
+
+  show_edit_predictions: true,
+
+  edit_predictions: {
+    mode: 'eager',
+  },
+};
+
+const keyMap = [
+  {
+    context: 'Editor',
+    bindings: {
+      'ctrl-c': 'editor::Copy',
+      'ctrl-x': 'editor::Cut',
+      'ctrl-v': 'editor::Paste',
+      'ctrl-shift-c': 'editor::CopyAndTrim',
+      'ctrl-shift-v': 'editor::Paste',
+      'cmd-c': 'editor::Copy',
+      'cmd-x': 'editor::Cut',
+      'cmd-v': 'editor::Paste',
+    },
+  },
+  {
+    context: 'Terminal',
+    bindings: {
+      'ctrl-shift-c': 'terminal::Copy',
+      'ctrl-shift-v': 'terminal::Paste',
+      'cmd-shift-c': 'terminal::Copy',
+      'cmd-shift-v': 'terminal::Paste',
+    },
+  },
+  {
+    context: 'Editor && edit_prediction',
+    bindings: {
+      tab: 'editor::AcceptEditPrediction',
+      'alt-tab': 'editor::AcceptEditPrediction',
+      'alt-l': null,
+    },
+  },
+  {
+    context: 'Editor && edit_prediction_conflict',
+    bindings: {
+      'alt-l': 'editor::AcceptEditPrediction',
+      tab: 'editor::ComposeCompletion',
+    },
+  },
+];
+
 if (!fs.existsSync('/root/.config/zed')) fs.mkdirSync('/root/.config/zed', { recursive: true });
-fs.copyFileSync(`${underpostRoot}/.vscode/zed.settings.json`, `/root/.config/zed/settings.json`);
-fs.copyFileSync(`${underpostRoot}/.vscode/zed.keymap.json`, `/root/.config/zed/keymap.json`);
+fs.writeFileSync(`/root/.config/zed/settings.json`, JSON.stringify(settings, null, 4), 'utf8');
+fs.writeFileSync(`/root/.config/zed/keymap.json`, JSON.stringify(keyMap, null, 4), 'utf8');
 
 shellExec(`ZED_ALLOW_ROOT=true zed ${process.argv[2] ? process.argv[2] : '.'}`);
 
