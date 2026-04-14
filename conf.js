a550968898ec9526ed3dceef48839ce6be3f1571
underpostnet <fcoverdugoa@underpost.net>
2026-04-14 18:37:39 -0400

    [client-core] Add SocketIoHandler and AppStore to DefaultConf component list

--- conf.js
+++ conf.js
@@ -63,6 +63,8 @@ const DefaultConf = /**/ {
           'Pagination',
           'windowGetDimensions',
           'SearchBox',
+          'SocketIoHandler',
+          'AppStore',
         ],
         default: [
           'MenuDefault',
