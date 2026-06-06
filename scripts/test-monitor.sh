a74316ae140c7917c2923f9f6f63d0ea74859116
underpostnet <fcoverdugoa@underpost.net>
2026-06-06 11:13:46 -0400

    [monitor] Add test-monitor script: Implement deployment and monitoring commands for development environment

---
+++ scripts/test-monitor.sh
@@ -0,0 +1,25 @@
+
+#!/usr/bin/env bash
+set -euo pipefail
+
+ENV=development
+DEPLOY_ID=dd-test
+IMAGE=underpost/wp:v3.2.14
+USE_CERT=false           # Set to true to use --cert, false to use --disable-update-proxy
+USE_PULL_BUNDLE=true     # Set to true to include --pull-bundle in start command, false to omit it
+VERSIONS=green
+LINK_CMD="cd /home/dd,underpost clone underpostnet/pwa-microservices-template-private,cd /home/dd/pwa-microservices-template-private,npm install,npm link"
+
+if [ "$USE_PULL_BUNDLE" = true ]; then
+    DEPLOY_CMD="$LINK_CMD,underpost secret underpost --create-from-env,underpost start --build --run --pull-bundle $DEPLOY_ID $ENV"
+else
+    DEPLOY_CMD="$LINK_CMD,underpost secret underpost --create-from-env,underpost start --build --run $DEPLOY_ID $ENV"
+fi
+
+if [ "$USE_CERT" = true ]; then
+    PROXY_FLAG="--cert"
+fi
+
+node bin deploy $DEPLOY_ID $ENV --kubeadm --sync --build-manifest --image $IMAGE --timeout-response 300000ms --versions $VERSIONS --replicas 1 --cmd "$DEPLOY_CMD"
+node bin deploy $DEPLOY_ID $ENV --kubeadm --disable-update-proxy $PROXY_FLAG
+node bin monitor $DEPLOY_ID $ENV --ready-deployment --promote --timeout-response 300000ms --versions $VERSIONS --replicas 1
