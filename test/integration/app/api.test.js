8ed062470a2cc916034beedab06c6607821aeb26
underpostnet <fcoverdugoa@underpost.net>
2026-08-23 16:49:51 -0400

    [test] Run the API test suite against an in-process server

--- test/integration/app/api.test.js
+++ test/integration/app/api.test.js
@@ -1,29 +1,48 @@
 'use strict';
 
 import axios from 'axios';
-import dotenv from 'dotenv';
+import express from 'express';
 
 import { expect } from 'chai';
+import { TestRouter } from '../../../src/api/test/test.router.js';
 import { loggerFactory } from '../../../src/server/ops/logger.js';
-
-dotenv.config();
-
-const PORT = parseInt(process.env.PORT) + 1;
 
 const logger = loggerFactory(import.meta);
 
 await logger.setUpInfo();
 
-const BASE_URL =
-  process.env.NODE_ENV === 'development'
-    ? `http://localhost:${PORT}/${process.env.BASE_API}`
-    : `https://www.nexodev.org/api`;
-
 describe(`GET 'Test' API Request `, async () => {
+  const app = express();
+  app.use('/api/test', TestRouter.router({ authMiddleware: (_req, _res, next) => next() }));
+
+  let server;
+  let baseUrl;
+
+  beforeAll(
+    () =>
+      new Promise((resolve, reject) => {
+        server = app.listen(0, '127.0.0.1', () => {
+          baseUrl = `http://127.0.0.1:${server.address().port}/api`;
+          resolve();
+        });
+        server.on('error', reject);
+      }),
+  );
+
+  afterAll(
+    () =>
+      new Promise((resolve, reject) =>
+        server.close((error) => {
+          if (error) reject(error);
+          else resolve();
+        }),
+      ),
+  );
+
   {
     const youtubeId = '2aib-pmgUdQ';
-    const url = `${BASE_URL}/test/youtube-id/?url=https://www.youtube.com/watch?v=${youtubeId}`;
     it(`youtube id from raw youtube url`, async () => {
+      const url = `${baseUrl}/test/youtube-id/?url=https://www.youtube.com/watch?v=${youtubeId}`;
       logger.info('request info', { url });
       const res = await axios.get(url);
       logger.info('response', res.data);
@@ -32,8 +51,8 @@ describe(`GET 'Test' API Request `, async () => {
   }
   {
     const email = 'test@gmail.com';
-    const url = `${BASE_URL}/test/verify-email/?email=${email}`;
     it(`valid format email`, async () => {
+      const url = `${baseUrl}/test/verify-email/?email=${email}`;
       logger.info('request info', { url });
       const res = await axios.get(url);
       logger.info('response', res.data);
@@ -42,8 +61,8 @@ describe(`GET 'Test' API Request `, async () => {
   }
   {
     const password = 'Password123!';
-    const url = `${BASE_URL}/test/is-strong-password/?password=${password}`;
     it(`valid password`, async () => {
+      const url = `${baseUrl}/test/is-strong-password/?password=${password}`;
       logger.info('request info', { url });
       const res = await axios.get(url);
       logger.info('response', res.data);
