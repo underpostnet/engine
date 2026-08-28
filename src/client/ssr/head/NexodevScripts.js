164cb07064b4eb792c587a636fe672a8d452cb1a
underpostnet <fcoverdugoa@underpost.net>
2026-08-27 20:27:35 -0400

    [client] Served the fullcalendar global build bundle

--- src/client/ssr/head/NexodevScripts.js
+++ src/client/ssr/head/NexodevScripts.js
@@ -7,7 +7,7 @@ SrrComponent = ({ ssrPath }) => html`
   <link rel="stylesheet" href="${ssrPath}dist/fullcalendar/skeleton.css" />
   <link rel="stylesheet" href="${ssrPath}dist/fullcalendar/themes/classic/theme.css" />
   <link rel="stylesheet" href="${ssrPath}dist/fullcalendar/themes/classic/palette.css" />
-  <script type="text/javascript" src="${ssrPath}dist/fullcalendar/global.js"></script>
+  <script type="text/javascript" src="${ssrPath}dist/fullcalendar/all/global.js"></script>
   <script type="text/javascript" src="${ssrPath}dist/fullcalendar/themes/classic/global.js"></script>
   <script type="text/javascript" src="${ssrPath}dist/@fullcalendar/rrule/global.js"></script>
   <script type="text/javascript" src="${ssrPath}dist/easymde/easymde.min.js"></script>
